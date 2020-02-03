b72ff3c52651f139d25795747018f2b88e910587
sven_beyel <sven.beyel@gv.hamburg.de>
2020-02-03 15:55:42 +0100

    add jsdoc for addons

--- einwohnerabfrage/resultView.js
+++ einwohnerabfrage/resultView.js
@@ -1,8 +1,25 @@
 import ResultTemplate from "text-loader!./resultTemplate.html";
 
-const ResultView = Backbone.View.extend({
+const ResultView = Backbone.View.extend(/** @lends ResultView.prototype */{
+    /**
+     * @class ResultView
+     * @extends Backbone.View
+     * @memberof Addons.Einwohnerabfrage
+     * @constructs
+     */
     model: {},
+
+    /**
+     * @member ResultTemplate
+     * @description Template used to create Resultview for Einwohnerabfrage
+     * @returns {void}
+     */
     template: _.template(ResultTemplate),
+
+    /**
+     * Renders the result view.
+     * @returns {void}
+     */
     render: function () {
         var attr = this.model.toJSON();
 
---
+++ sdpdownload/jsdoc/events.js
@@ -0,0 +1,9 @@
+/**
+ * @event Addons.SdpDownloadModel#changeIsActive
+ * @description Trigger by SdpDownload is activated.
+ */
+
+/**
+ * @event Addons.SdpDownloadModel#render
+ * @description Renders the SdpDownload.
+ */
