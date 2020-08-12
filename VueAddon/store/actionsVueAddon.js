3e964cfe278a24aba01a9480ac9384fea8daa650
sven_beyel <sven.beyel@gv.hamburg.de>
2020-08-12 13:54:51 +0200

    BG-779 update vue addon to use with ToolManager

--- VueAddon/store/actionsVueAddon.js
+++ VueAddon/store/actionsVueAddon.js
@@ -1,18 +1,6 @@
-import {fetchFirstModuleConfig} from "../../../src/utils/fetchFirstModuleConfig";
-
-/** @const {String} [Path array of possible config locations. First one found will be used] */
 /** @const {object} [vue actions] */
-const configPaths = [
-    "configJson.Portalconfig.menu.tools.children.VueAddon"
-];
 
 export default {
-    /**
-     * Sets the config-params of this tool into state.
-     * @param {object} context the context Vue instance
-     * @returns {boolean} false, if config does not contain the tool
-     */
-    initialize: context => fetchFirstModuleConfig(context, configPaths, "vueaddon"),
     /**
      * Checks if this tool should be open initially controlled by the url param "isinitopen".
      * @returns {void}
