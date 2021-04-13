f7a87e74bb3df413e131a6b65a8da320820a0ccf
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-04-13 20:00:09 +0200

    update - featuresList: non sortable col

--- cosi/FeaturesList/components/FeaturesList.vue
+++ cosi/FeaturesList/components/FeaturesList.vue
@@ -29,7 +29,8 @@ export default {
                 {
                     text: "Icon",
                     value: "style",
-                    filterable: false
+                    filterable: false,
+                    sortable: false
                 },
                 {
                     text: "Einrichtung",
