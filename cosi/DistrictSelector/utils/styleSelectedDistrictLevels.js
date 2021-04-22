f3ee4ff479a0e2201a3f7d369124c52cf16dad1f
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-04-22 12:02:13 +0200

    update - styleSelectedDistrictLevels: defaultLayerStyles structure as ol/style

--- cosi/DistrictSelector/utils/styleSelectedDistrictLevels.js
+++ cosi/DistrictSelector/utils/styleSelectedDistrictLevels.js
@@ -1,14 +1,14 @@
 import {Fill, Stroke, Style} from "ol/style.js";
 import {asArray} from "ol/color";
 
-const defaultsStyleValues = {fillColor: [255, 255, 255, 0], stroke: {color: "#3399CC", width: 3}};
+const defaultsStyleValues = {fill: {color: [255, 255, 255, 0]}, stroke: {color: "#3399CC", width: 3}};
 
 /**
  * @description Styles the features of the selected layer with transparent overlay for better readability
  * @param {Object[]} districtLevels - the districtlevels object from the district selector module
  * @param {string|undefined} selectedLevelId - the ID of the selected level
  * @param {number} [opacity=0.6] - the opacity of the overlay
- * @param {Object} [defaults={fillColor: [255, 255, 255, 0], stroke: {color: "#3399CC", width: 3}}] - the default values in case no styles can be obtained
+ * @param {Object} [defaults={fill: {color: [255, 255, 255, 0]}, stroke: {color: "#3399CC", width: 3}}] - the default values in case no styles can be obtained
  * @returns {void}
  */
 export default function styleSelectedDistrictLevels (districtLevels, selectedLevelId, opacity = 0.6, defaults = defaultsStyleValues) {
@@ -25,12 +25,15 @@ export default function styleSelectedDistrictLevels (districtLevels, selectedLev
         if (level.layerId === selectedLevelId) {
             const vectorLayerStyle = level.layer.getStyleFunction(), // read styling function from layer
                 baseStyle = vectorLayerStyle(level.layer.getSource().getFeatures()[0])?.[0], // apply to first feature of layer
-                baseFillColor = asArray(baseStyle?.getFill()?.getColor() || defaults.fillColor), // read base fill color of style
+                baseFillColor = asArray(baseStyle?.getFill()?.getColor() || defaults.fill?.color || defaultsStyleValues.fill.color), // read base fill color of style
                 selectedStyle = new Style({
                     fill: new Fill({
                         color: [baseFillColor[0], baseFillColor[1], baseFillColor[2], opacity]
                     }),
-                    stroke: baseStyle?.getStroke() || new Stroke(defaults.stroke)
+                    stroke: baseStyle?.getStroke() || new Stroke({
+                        color: defaults.stroke?.color || defaultsStyleValues.stroke.color,
+                        width: defaults.stroke?.width || defaultsStyleValues.stroke.width
+                    })
                 });
 
             level.layer.setStyle(selectedStyle);
