7cf557af2a4ec59053dadb684a9f8ed62ed6962b
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-05-31 13:20:34 +0200

    add - arrayIsEqual utils... maybe obsolete?

---
+++ cosi/utils/arrayIsEqual.js
@@ -0,0 +1,27 @@
+/**
+ * compares to lists for equality
+ * @param {*} arr1 - the 1st list to compare
+ * @param {*} arr2 - the 2nd list to compare
+ * @param {Boolean} ordered - is the list order important?
+ * @returns {Boolean} are the arrays equal?
+ */
+export default function arrayIsEqual (arr1, arr2, ordered = false) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    if (!ordered) {
+        for (const el of arr1) {
+            if (!arr2.find(_el => _el === el)) {
+                return false;
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
