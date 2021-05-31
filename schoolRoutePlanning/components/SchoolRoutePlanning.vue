fb4d3e13f0892afcd73b39abbd94bf856fd21880
richard_schmid <richard.schmid@gv.hamburg.de>
2021-05-31 16:21:52 +0200

    added width for mobile

--- schoolRoutePlanning/components/SchoolRoutePlanning.vue
+++ schoolRoutePlanning/components/SchoolRoutePlanning.vue
@@ -178,6 +178,7 @@ export default {
         :resizable-window="resizableWindow"
         :deactivateGFI="deactivateGFI"
         :initialWidth="initialWidth"
+        :initialWidthMobile="300"
     >
         <template v-slot:toolBody>
             <div
