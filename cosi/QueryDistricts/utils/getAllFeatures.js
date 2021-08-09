d878601fc7f0b7fe93fb555fce24d563e165ea15
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-08-09 16:46:59 +0200

    rm console.logs and comments

--- cosi/QueryDistricts/utils/getAllFeatures.js
+++ cosi/QueryDistricts/utils/getAllFeatures.js
@@ -5,23 +5,15 @@ import {getFeature} from "../../../../src/api/wfs/getFeature.js";
 /**
  * Returns all features of a layer by the given layer id.
  * If the features are not yet stored, it will be loaded.
- * @param {Object} featureList - The key is the layer id and the value are the features.
  * @param {String} layerId - The id of the layer.
  * @returns {module:ol/Feature[]} The features of the layer.
  */
 export async function getAllFeatures (layerId) {
-    // if (Object.prototype.hasOwnProperty.call(featureList, layerId)) {
-    //     return featureList[layerId];
-    // }
-
-    // console.log(featureList, layerId);
     const layer = getLayerWhere({id: layerId}),
         wfsReader = new WFS({
             featureNS: layer.featureNS
         }),
         features = await getFeature(layer?.url, layer?.featureType);
-
-    // featureList[layerId] = wfsReader.readFeatures(features);
 
     return wfsReader.readFeatures(features);
 }
