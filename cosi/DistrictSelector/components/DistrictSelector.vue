c59b8f355b2ea1e6b59c9268d6335e75371e0948
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-09-20 23:01:31 +0200

    wait to update extent by tick

--- cosi/DistrictSelector/components/DistrictSelector.vue
+++ cosi/DistrictSelector/components/DistrictSelector.vue
@@ -329,7 +329,7 @@ export default {
                 });
                 this.$nextTick(() => {
                     this.updateExtent();
-                })
+                });
             }
         },
 
