e479955d79496d2d1a128d87a5592bcc14d046d3
dennisrpnw <dennis.rupnow@outlook.com>
2021-09-29 14:45:24 +0200

    predefined styles file, removes deprecated stuff

--- cosi/AccessibilityAnalysis/utils/exportResults.js
+++ cosi/AccessibilityAnalysis/utils/exportResults.js
@@ -1,11 +1,8 @@
 import {featuresToGeoJsonCollection, featureToGeoJson} from "../../utils/geomUtils";
 import {downloadJsonToFile} from "../../utils/download";
 import store from "../../../../src/app-store";
-import methods from "../components/methodsAnalysis";
 import {Point} from "ol/geom.js";
 import Feature from "ol/Feature.js";
-import * as Proj from "ol/proj.js";
-import * as Extent from "ol/extent";
 
 /**
  * Gets the map's CRS from the app-store
@@ -16,36 +13,6 @@ function getPortalCrs () {
 }
 
 /**
- * Gets the coordinates of the selected facility
- * @param {*} selectedFacilityName - the selected facility for analysis
- * @param {*} isFeatureDisabled - isFeatureDisabled function
- * @returns {Array} the coordinates of all facilities
- */
-function getCoordinates (selectedFacilityName, isFeatureDisabled) {
-    const selectedLayerModel = Radio.request("ModelList", "getModelByAttributes", {
-        name: selectedFacilityName,
-        type: "layer"
-    });
-
-    if (selectedLayerModel) {
-        const features = selectedLayerModel.get("layer")
-            .getSource().getFeatures()
-            .filter(f => (typeof f.style_ === "object" || f.style_ === null) && !isFeatureDisabled(f));
-
-        return features
-            .map((feature) => {
-                const geometry = feature.getGeometry();
-
-                if (geometry.getType() === "Point") {
-                    return geometry.getCoordinates().splice(0, 2);
-                }
-                return Extent.getCenter(geometry.getExtent());
-
-            }).map(coord => Proj.transform(coord, "EPSG:25832", "EPSG:4326"));
-    }
-    return null;
-}
-/**
  * Exports the results of the supply analysis as geojson
  * @param {*} mapLayer - the components mapLayer
  * @param {*} coordinate - eventual coordinate
@@ -53,16 +20,14 @@ function getCoordinates (selectedFacilityName, isFeatureDisabled) {
  * @param {*} isFeatureDisabled - isFeatureDisabled function
  * @returns {void}
  */
-export function exportAsGeoJson (mapLayer, coordinate, selectedFacilityName, isFeatureDisabled) {
+export function exportAsGeoJson (mapLayer, coordinate) {
     const projectionCode = getPortalCrs(),
         featureCollection = featuresToGeoJsonCollection(mapLayer.getSource().getFeatures(), false, projectionCode),
         stroke = {
             color: "White",
             width: 1
         },
-        styles = [],
-        colors = methods.getFeatureColors(),
-        stylesJson = {};
+        colors = this.getFeatureColors();
 
     for (let i = 0; i < featureCollection.features.length; i++) {
         const style = {
@@ -73,12 +38,8 @@ export function exportAsGeoJson (mapLayer, coordinate, selectedFacilityName, isF
         };
 
         featureCollection.features[i].style = style;
-        styles.push(style);
+        featureCollection.features[i].properties.index = i;
     }
-
-    stylesJson.outer = styles[0];
-    stylesJson.middle = styles[1];
-    stylesJson.inner = styles[2];
 
     if (coordinate !== null) {
         const feature = new Feature(new Point(coordinate)),
@@ -87,8 +48,7 @@ export function exportAsGeoJson (mapLayer, coordinate, selectedFacilityName, isF
         featureCollection.features.push(featureGeoJson);
     }
     else {
-        // TODO: try to use the getCoordinates of methodsAnalysis.js - "this" has wrong context and the function will return null
-        const coordinates = getCoordinates(selectedFacilityName, isFeatureDisabled);
+        const coordinates = this.getCoordinates();
 
         for (let i = 0; i < coordinates.length; i++) {
             const feature = new Feature(new Point(coordinates[i])),
@@ -98,5 +58,5 @@ export function exportAsGeoJson (mapLayer, coordinate, selectedFacilityName, isF
         }
     }
     downloadJsonToFile(featureCollection, "Erreichbarkeitsanalyse_CoSI.geojson");
-    downloadJsonToFile(stylesJson, "Erreichbarkeitsanalyse_CoSI_styles.json");
+    downloadJsonToFile(require("../assets/Erreichbarkeitsanalyse_style.json"), "Erreichbarkeitsanalyse_CoSI_styles.json");
 }
