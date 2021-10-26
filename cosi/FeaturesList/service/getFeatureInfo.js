ec3718208e9c27caface4b58b44fd0207565e070
Christian Bähnisch <baehnisch@informatik.uni-hamburg.de>
2021-10-26 10:27:58 +0200

    getFeatureInfo

--- cosi/FeaturesList/service/getFeatureInfo.js
+++ cosi/FeaturesList/service/getFeatureInfo.js
@@ -1,43 +1,52 @@
 import axios from "axios";
-
+import TileWMS from "ol/source/TileWMS.js";
+import TileGrid from "ol/tilegrid/TileGrid.js";
 
 /**
  *
- * @export
- * @return {*}
+ * @param {*} url url
+ * @param {*} layer layer
+ * @return {*}  TileWMs
  */
-export async function getFeatureInfo () {
-    // const url = "https://geodienste.hamburg.de/HH_WMS_Strassenverkehr?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&QUERY_LAYERS=strassenverkehr_tag_abend_nacht_2017&CACHEID=7186304&LAYERS=strassenverkehr_tag_abend_nacht_2017&INFO_FORMAT=text%2Fxml&FEATURE_COUNT=1&I=442&J=115&WIDTH=512&HEIGHT=512&CRS=EPSG%3A25832&BBOX=567090.5995497429%2C5939115.663070875%2C567158.3328465002%2C5939183.396367633";
-
-    // ?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=strassenverkehr_tag_abend_nacht_2017&CACHEID=7186304&LAYERS=strassenverkehr_tag_abend_nacht_2017&INFO_FORMAT=text%2Fxml&FEATURE_COUNT=1&I=442&J=115&WIDTH=512&HEIGHT=512&CRS=EPSG%3A25832&STYLES=&
-    const url = "https://geodienste.hamburg.de/HH_WMS_Strassenverkehr";
-
-
-    return axios.get(url, {
-        // params: {
-        //     service: "WMS",
-        //     version: "1.3.0",
-        //     request: "GetFeature",
-
-        //     bbox: 567090.5995497429, 5939115.663070875, 567158.3328465002, 5939183.396367633
-        // }
-
-        params: {
-            "SERVICE": "WMS",
-            "VERSION": "1.3.0",
-            "REQUEST": "GetFeatureInfo",
-            "QUERY_LAYERS": "strassenverkehr_tag_abend_nacht_2017",
-            "LAYERS": "strassenverkehr_tag_abend_nacht_2017",
-            "INFO_FORMAT": "text/xml",
-            "FEATURE_COUNT": "1",
-            "I": "442",
-            "J": "115",
-            "WIDTH": "512",
-            "HEIGHT": "512",
-            "CRS": "EPSG:25832",
-            "BBOX": "567090.5995497429,5939115.663070875,567158.3328465002,5939183.396367633"
-        }
+function createTileWMS (url, layer) {
+    return new TileWMS({
+        url,
+        gutter: 0,
+        params: {"LAYERS": layer, "FORMAT": "image/png", "VERSION": "1.3.0"},
+        tileGrid: new TileGrid({
+            resolutions: [0.1],
+            origin: [
+                0,
+                0
+            ],
+            tileSize: 512
+        })
     });
-
 }
 
+/**
+ *
+ * @param {*} source source
+ * @param {*} coord coordinate
+ * @param {*} projection projection
+ * @return {*} url
+ */
+function getFeatureInfoUrl (source, coord, projection) {
+    return source.getFeatureInfoUrl(coord, null, projection, {"INFO_FORMAT": "text/xml"});
+}
+
+/**
+ *
+ * @param {*} wmsUrl wmsUrl
+ * @param {*} layer layer
+ * @param {*} coord coord
+ * @param {*} projection projection
+ * @return {*} feature info
+ */
+export async function getFeatureInfo (wmsUrl, layer, coord, projection) {
+    const source = createTileWMS(wmsUrl, layer),
+        url = getFeatureInfoUrl(source, coord, projection),
+        ret = await axios.get(url);
+
+    return ret.data;
+}
--- cosi/FeaturesList/tests/unit/service/getFeatureInfo.spec.js
+++ cosi/FeaturesList/tests/unit/service/getFeatureInfo.spec.js
@@ -2,19 +2,17 @@
 import {getFeatureInfo} from "../../../service/getFeatureInfo";
 import {expect} from "chai";
 
-import TileWMS from "ol/source/TileWMS.js";
-import TileGrid from "ol/tilegrid/TileGrid.js";
 
 const expectedXML = `<?xml version='1.0' encoding='UTF-8'?>
 <FeatureCollection xmlns="http://www.opengis.net/wfs" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:gml="http://www.opengis.net/gml" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd http://www.deegree.org/app https://geodienste.hamburg.de/HH_WMS_Strassenverkehr?request=GetFeatureInfoSchema&amp;layers=strassenverkehr_tag_abend_nacht_2017">
   <gml:boundedBy>
     <gml:Box srsName="EPSG:25832">
-      <gml:coordinates decimal="." cs="," ts=" ">566285.000,5938115.000 568675.000,5939635.000</gml:coordinates>
+      <gml:coordinates decimal="." cs="," ts=" ">564285.000,5937835.000 564325.000,5937875.000</gml:coordinates>
     </gml:Box>
   </gml:boundedBy>
   <gml:featureMember>
-    <app:strassenverkehr_tag_abend_nacht_2017 xmlns:app="http://www.deegree.org/app" fid="APP_STRASSENVERKEHR_TAG_ABEND_NACHT_2017_74664">
-      <app:klasse>&gt; 70 - 75 dB(A)</app:klasse>
+    <app:strassenverkehr_tag_abend_nacht_2017 xmlns:app="http://www.deegree.org/app" fid="APP_STRASSENVERKEHR_TAG_ABEND_NACHT_2017_16872">
+      <app:klasse>&gt; 55 - 60 dB(A)</app:klasse>
     </app:strassenverkehr_tag_abend_nacht_2017>
   </gml:featureMember>
 </FeatureCollection>`;
@@ -22,33 +20,11 @@ const expectedXML = `<?xml version='1.0' encoding='UTF-8'?>
 describe.only("getFeatureInfo", () => {
 
     it("should return feature info", async () => {
-        // const ret = await getFeatureInfo();
+        const featureInfo = await getFeatureInfo("https://geodienste.hamburg.de/HH_WMS_Strassenverkehr",
+            "strassenverkehr_tag_abend_nacht_2017",
+            [564310.8970702873, 5937870.667724043],
+            "EPSG:25832");
 
-        // console.log(ret.data);
-
-        // expect(ret.data).to.be.equal(expectedXML);
-
-        const source = new TileWMS({
-                url: this.get("url"),
-                gutter: 0,
-                params: {"CACHEID": 1544879, "LAYERS": "strassenverkehr_tag_abend_nacht_2017", "FORMAT": "image/png", "VERSION": "1.3.0", "TRANSPARENT": "true"},
-                tileGrid: new TileGrid({
-                    resolutions: [66.14579761460263, 26.458319045841044, 15.874991427504629, 10.583327618336419, 5.2916638091682096, 2.6458319045841048, 1.3229159522920524, 0.6614579761460262, 0.2645831904584105, 0.13229159522920522],
-                    origin: [
-                        442800,
-                        5809000
-                    ],
-                    tileSize: 512
-                })
-            }),
-
-            url = wmsSource.getFeatureInfoUrl(
-                evt.coordinate,
-                viewResolution,
-                "EPSG:3857",
-                {"INFO_FORMAT": "text/html"}
-            );
-
-        console.log(url);
+        expect(featureInfo).to.be.eql(expectedXML);
     });
 });
