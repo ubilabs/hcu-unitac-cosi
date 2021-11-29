c3c95adc90d922b72c6e9f0689628a4410f2d9f3
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2021-11-29 15:27:59 +0100

    rm old sdpdownload

--- sdpDownload/store/gettersSdpDownload.js
+++
@@ -1,9 +0,0 @@
-
-import {generateSimpleGetters} from ".../../../src/app-store/utils/generators";
-import sdpAddonState from "./stateSdpDownload";
-
-const getters = {
-    ...generateSimpleGetters(sdpAddonState)
-};
-
-export default getters;
