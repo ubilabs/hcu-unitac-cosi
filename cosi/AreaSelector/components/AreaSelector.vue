245ba6b6475c45419b8e85cfc7e099dc222cc679
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2022-02-16 11:15:47 +0100

    merge masterportal 2.18, TODO: bugfixes

--- cosi/AreaSelector/components/AreaSelector.vue
+++ cosi/AreaSelector/components/AreaSelector.vue
@@ -1,5 +1,5 @@
 <script>
-import Tool from "../../../../src/modules/tools/Tool.vue";
+import Tool from "../../../../src/modules/tools/ToolTemplate.vue";
 import ToolInfo from "../../components/ToolInfo.vue";
 import getComponent from "../../../../src/utils/getComponent";
 import {mapGetters, mapMutations, mapActions} from "vuex";
