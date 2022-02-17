723bfec36fd1b0fafda7748f9b3123046da7e8ba
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2022-02-17 12:25:04 +0100

    DistrictSelector: fallback to areaSelector geom, ReverseGeocoder: fallback to NOMINATIM

--- cosi/utils/geocode.js
+++ cosi/utils/geocode.js
@@ -1,29 +1,33 @@
 import {fetchRoutingNominatimGeosearchReverse} from "../../../src/modules/tools/routing/utils/geosearch/routing-nominatim-geosearch";
+import {fetchRoutingBkgGeosearchReverse} from "../../../src/modules/tools/routing/utils/geosearch/routing-bkg-geosearch";
 import * as Proj from "ol/proj.js";
 
 /**
  * @param {Array<Number>} coords - the point coordinates
  * @param {String} portalCrs - the portal's CRS
+ * @param {"NOMINATIM" | "BKG"} [type="NOMINATIM"] - which geocoder to use
  * @return {String} the address from gazetter
  */
-export function reverseGeocode (coords, portalCrs) {
+export function reverseGeocode (coords, portalCrs, type = "NOMINATIM") {
     const _coords = Proj.transform(coords, portalCrs, "EPSG:4326");
 
-    return fetchRoutingNominatimGeosearchReverse(_coords);
+    return type === "BKG" ? fetchRoutingBkgGeosearchReverse(_coords) : fetchRoutingNominatimGeosearchReverse(_coords);
 }
 
 /**
  * returns the address name for a geometry
  * @param {module:ol/Geometry} geom - the geometry
  * @param {String} portalCrs - the portal's CRS
+ * @param {"NOMINATIM" | "BKG"} [type="NOMINATIM"] - which geocoder to use
  * @return {String} the address
  */
-export async function getAddress (geom, portalCrs) {
+export async function getAddress (geom, portalCrs, type) {
+    console.log(type);
     const
         coordinates = geom.getType() === "Polygon" || geom.getType() === "MultiPolygon" ?
             geom.getExtent().getCenter() :
             geom.getCoordinates(),
-        geocode = await reverseGeocode(coordinates, portalCrs);
+        geocode = await reverseGeocode(coordinates, portalCrs, type);
 
     return geocode?.displayName;
 }
