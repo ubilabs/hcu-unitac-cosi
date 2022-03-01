5f4b8c0368705787cd3c03da7a631ec0d55dc017
Vilma Jochim <Jochimvi-lokal@LGV-W-D756261.fhhnet.stadt.hamburg.de>
2022-03-01 10:07:32 +0100

    BG-1869 CalculationComponent implemented

--- borisVue/components/BorisVue.vue
+++ borisVue/components/BorisVue.vue
@@ -4,14 +4,14 @@ import {mapGetters, mapActions, mapMutations} from "vuex";
 import getters from "../store/gettersBorisVue";
 import mutations from "../store/mutationsBorisVue";
 import DetailInformation from "./DetailInformation.vue";
-import CalculationSelect from "./CalculationSelect.vue";
+import CalculationComponent from "./CalculationComponent.vue";
 
 export default {
     name: "BorisVue",
     components: {
         Tool,
         DetailInformation,
-        CalculationSelect
+        CalculationComponent
     },
     computed: {
         ...mapGetters("Tools/BorisVue", Object.keys(getters)),
@@ -288,88 +288,64 @@ export default {
                             <div
                                 v-if="selectedBrwFeature.get('zBauweise')"
                             >
-                                <CalculationSelect
+                                <CalculationComponent
                                     :title="'Anbauart:'"
                                     :options="bauweisen"
                                     :selected-brw-feature="selectedBrwFeature"
                                     :text-ids="textIds"
                                     :text="'Wählen Sie die Bauweise Ihres Gebäudes aus der Liste aus: <strong>Einzelhäuser </strong> sind freistehende Häuser, die nicht an die Grundstücksgrenze, Nutzungsgrenze oder andere Häuser angebaut sind. Lediglich zu einer Seite darf der Raum zwischen Haus und Grundstücksgrenze mit Nebengebäuden, z. B. Garagen zugebaut sein. <strong> Ein Doppelhaus </strong>ist eine Kombination zweier Häuser, die beide einseitig auf eine gemeinsame seitliche Grundstücksgrenze oder Nutzungsgrenze (bei Wohnungs-/Teileigentum) gebaut sind. Zur Vermeidung von Missverständnissen werden die einzelnen Häuser als <strong>halbe Doppelhäuser oder Doppelhaushälften</strong> bezeichnet. <strong>Ein Endreihenhaus</strong> ist einseitig bzw. ein <strong>Mittelreihenhaus</strong> ist beidseitig auf die seitlichen Grundstücks- bzw. Nutzungsgrenzen gebaut, so dass sich Zeilen von mindestens drei Häusern und bis zu 50 Meter Länge ergeben. <strong>Die geschlossenen Bauweise</strong> kennzeichnet Gebäude, die zu allen Seiten keinen Grenzabstand aufweisen und vollständig umbaut sind. <strong>Die abweichende Bauweise</strong> bezeichnet alle sonstigen Gebäudestellungen, die nicht in den zuvor genannten Kategorien aufgehen. Beispielsweise gehören hierzu Gartenhofhäuser, die zusammen mit Nachbarhäusern, Nebengebäuden und geschosshohen Mauern einen Garten in einem Gartenhof umschließen.'"
                                     :toggle-info-text="toggleInfoText"
-                                    :selection-type="'zBauweise'"
-                                    :handle-option-change="handleOptionChange"
+                                    :handle-change="handleOptionChange"
+                                    :subject="'zBauweise'"
+                                    :type="'select'"
                                 />
                             </div>
                             <div
                                 v-if="selectedBrwFeature.get('zStrassenLage')"
                             >
-                                <CalculationSelect
+                                <CalculationComponent
                                     :title="'Lage zur Straße:'"
                                     :options="strassenlagen"
                                     :selected-brw-feature="selectedBrwFeature"
                                     :text-ids="textIds"
                                     :text="'Wählen Sie die Stellung und damit auch die Zuwegung Ihres Grundstücks zur Straße aus der Liste aus: Während bei <strong>Frontlage</strong> das Grundstück unmittelbar an genau eine Straße heranreicht, ist bei einer <strong>Ecklage</strong> eine unmittelbare Anbindung an mindestens zwei Straßen gegeben. Ein <strong>Pfeifenstielgrundstück</strong> ist eine schmale, pfeifenstielartige Zuwegung zu einem Grundstück, das nicht direkt an der Straße gelegen ist. Der Pfeifenstiel steht normalerweise im Alleineigentum des Pfeifenkopf-Grundstücks. Es ist jedoch auch möglich, dass ein Pfeifenstiel bis zu vier rückwärtige Grundstücke erschließt. <strong>Die Hinterlage</strong> bezeichnet ein rückwärtiges Grundstück, welches sich nicht im Eigentum des Grundstücks befindet, sondern über ein grundbuchliches Wegerecht oder als Baulast gesichert ist.'"
                                     :toggle-info-text="toggleInfoText"
-                                    :selection-type="'zStrassenLage'"
-                                    :handle-option-change="handleOptionChange"
+                                    :handle-change="handleOptionChange"
+                                    :subject="'zStrassenLage'"
+                                    :type="'select'"
                                 />
                             </div>
                             <div
                                 v-if="selectedBrwFeature.get('zGeschossfl_zahl')"
                             >
-                                <dt>
-                                    <span>Wertrelevante Geschossflächenzahl (WGFZ):</span>
-                                    <span 
-                                        class="glyphicon glyphicon-question-sign"
-                                        @click="toggleInfoText(4)"
-                                    />
-                                </dt>
-                                <dd>
-                                    <label>
-                                        <input
-                                            id="zGeschossfl_zahlInput"
-                                            type="text"
-                                            class="form-control"
-                                            :value="selectedBrwFeature.get('zGeschossfl_zahl').toString().replace('.', ',')"
-                                            @change="handleInputChange($event, 'zGeschossfl_zahl')"
-                                        >
-                                    </label>
-                                    <div
-                                        v-if="Object.values(textIds).includes(4)"
-                                        class="help"
-                                    >
-                                        <span>Die <strong>wertrelevante Geschossflächenzahl (WGFZ)</strong> wird über das Verhältnis der Geschossflächen zur Grundstücksfläche definiert. Geben Sie hier die WGFZ Ihres Grundstücks ein.</span>
-                                        <br>
-                                    </div>
-                                </dd>
+                                <CalculationComponent
+                                    :title="'Wertrelevante Geschossflächenzahl (WGFZ):'"
+                                    :options="[]"
+                                    :selected-brw-feature="selectedBrwFeature"
+                                    :text-ids="textIds"
+                                    :text="'Die <strong>wertrelevante Geschossflächenzahl (WGFZ)</strong> wird über das Verhältnis der Geschossflächen zur Grundstücksfläche definiert. Geben Sie hier die WGFZ Ihres Grundstücks ein.'"
+                                    :toggle-info-text="toggleInfoText"
+                                    :handle-change="handleInputChange"
+                                    :subject="'zGeschossfl_zahl'"
+                                    :type="'input'"
+                                />
                             </div>
                             <div
                                 v-if="selectedBrwFeature.get('zGrdstk_flaeche')"
                             >
-                                <dt>
-                                    <span>Grundstücksfläche in m²:</span>
-                                    <span 
-                                        class="glyphicon glyphicon-question-sign" 
-                                        @click="toggleInfoText(5)"
-                                    />
-                                </dt>
-                                <dd>
-                                    <label>
-                                        <input
-                                            id="zGrdstk_flaecheInput"
-                                            type="text"
-                                            class="form-control"
-                                            :value="selectedBrwFeature.get('zGrdstk_flaeche').toString().replace('.', ',')"
-                                            @change="handleInputChange($event, 'zGrdstk_flaeche')"
-                                        >
-                                    </label>
-                                    <div
-                                        v-if="Object.values(textIds).includes(5)"
-                                        class="help"
-                                    >
-                                        <span>Geben Sie für die <strong>Grundstücksfläche</strong> die Grundfläche Ihres Grundstücks laut Angabe im Liegenschaftskataster ein.</span>
-                                    </div>
-                                </dd>
+                                <CalculationComponent
+                                    :title="'Grundstücksfläche in m²:'"
+                                    :options="[]"
+                                    :selected-brw-feature="selectedBrwFeature"
+                                    :text-ids="textIds"
+                                    :text="'Geben Sie für die <strong>Grundstücksfläche</strong> die Grundfläche Ihres Grundstücks laut Angabe im Liegenschaftskataster ein.'"
+                                    :toggle-info-text="toggleInfoText"
+                                    :handle-change="handleInputChange"
+                                    :subject="'zGrdstk_flaeche'"
+                                    :type="'input'"
+                                />
                             </div>
+                            <!-- CalculationComponent Ende -->
                             <dt>
                                 <span>Ihr umgerechneter Bodenrichtwert:</span>
                                 <span 
---
+++ borisVue/components/CalculationComponent.vue
@@ -0,0 +1,144 @@
+<script>
+import {mapActions, mapMutations} from "vuex";
+import mutations from "../store/mutationsBorisVue";
+
+export default {
+    name: "CalculationComponent",
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        // only for select, if input oins BorisVue.vue --> :optopns: []
+        options: {
+            type: Array,
+            required: true
+        },
+        selectedBrwFeature: {
+            type: Object,
+            required: true
+        },
+        textIds: {
+            type: Array,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        toggleInfoText: {
+            type: Function,
+            required: true
+        },
+        handleChange: {
+            type: Function,
+            required: true
+        },
+        // zGrdstk_flaeche, zGeschossfl_zahl, zStrassenLage, zBauweise
+        subject: {
+            type: String,
+            required: true
+        },
+        // select or input
+        type: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        ...mapActions("Tools/BorisVue", ["updateSelectedBrwFeature", "sendWpsConvertRequest"]),
+        ...mapMutations("Tools/BorisVue", Object.keys(mutations)),
+        // to check if Einzelhäuser or Doppelhäuser are selected to change the option name to singular
+        checkForBauweiseMatch (option) {
+
+            let zBauweise = this.selectedBrwFeature.get("zBauweise");
+
+            if (this.selectedBrwFeature.get("zBauweise") === "eh Einzelhäuser") {
+                zBauweise = "eh Einzelhaus (freistehend)";
+            }
+            else if (this.selectedBrwFeature.get("zBauweise") === "dh Doppelhaushälften") {
+                zBauweise = "dh Doppelhaushälfte";
+            }
+            return option === zBauweise;
+        }
+    }
+
+};
+</script>
+
+<template>
+    <div>
+        <!-- CalculationSelect -->
+        <div v-if="type === 'select'">
+            <dt>
+                <span>{{ title }}</span>
+                <span
+                    class="glyphicon glyphicon-question-sign" 
+                    @click="toggleInfoText(text)"
+                />
+            </dt>
+            <dd>
+                <select
+                    class="form-control"
+                    @change="handleChange($event, subject)"
+                >
+                    <option
+                        v-for="(option, i) in options"
+                        :key="i"
+                        :value="option"
+                        :SELECTED="checkForBauweiseMatch(option)"
+                    >
+                        {{ option }}
+                    </option>
+                </select>
+                <div
+                    v-if="Object.values(textIds).includes(text)"
+                    class="help"
+                >
+                    <span v-html="text" />
+                    <br>
+                </div>
+            </dd>
+        </div>
+        <!-- CalculationInput -->
+        <div v-else>
+            <dt>
+                <span>{{ title }}</span>
+                <span
+                    class="glyphicon glyphicon-question-sign" 
+                    @click="toggleInfoText(text)"
+                />
+            </dt>
+            <dd>
+                <label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        :value="selectedBrwFeature.get(subject).toString().replace('.', ',')"
+                        @change="handleChange($event, subject)"
+                    >
+                </label>
+                <div
+                    v-if="Object.values(textIds).includes(text)"
+                    class="help"
+                >
+                    <span v-html="text" />
+                    <br>
+                </div>
+            </dd>
+        </div>
+    </div>
+</template>
+
+<style  lang="scss" scoped>
+    dt {
+        background-color: rgba(227, 227, 227, 0.5);
+        font-family: "UniversNextW04-620CondB", "Arial Narrow", Arial, sans-serif;
+        padding: 8px;
+    }
+    dd{
+        padding: 8px;
+        word-wrap: break-word;
+    }
+
+</style>
