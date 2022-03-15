ac79d46d6e7aa447663bd7cb04c55dd65c13aba7
Vilma Jochim <Jochimvi-lokal@LGV-W-D756261.fhhnet.stadt.hamburg.de>
2022-03-15 15:06:20 +0100

    BG-1869 Inkas comments and suggestions included

--- borisVue/store/actionsBorisVue.js
+++ borisVue/store/actionsBorisVue.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import helpers from "../utils/helpers";
 import thousandsSeparator from "../../../src/utils/thousandsSeparator";
 import {WFS, WMSGetFeatureInfo} from "ol/format.js";
-import WPS from "../../../src/api/wps";
 import mapCollection from "../../../src/core/dataStorage/mapCollection";
 
 const actions = {
@@ -70,7 +69,7 @@ const actions = {
         commit("setSelectedBrwFeature", {});
         dispatch("MapMarker/removePolygonMarker", null, {root: true});
         dispatch("MapMarker/removePointMarker", null, {root: true});
-        commit("setTextId", []);
+        commit("setTextIds", []);
 
         commit("setSelectedBrwFeature", {});
         dispatch("MapMarker/removePolygonMarker", null, {root: true});
@@ -98,7 +97,7 @@ const actions = {
     */
     async checkBrwFeature ({commit, dispatch, state}, {brwFeatures, year}) {
         if (brwFeatures !== undefined) {
-            const brwFeatureByYear = await dispatch("findBrwFeatureByYear", {features: state.selectedBrwFeature, year});
+            const brwFeatureByYear = await helpers.findBrwFeatureByYear({features: state.selectedBrwFeature, year});
 
             if (brwFeatureByYear === undefined) {
                 commit("setSelectedPolygon", null);
@@ -343,7 +342,7 @@ const actions = {
 
         if (status === 200) {
             const features = new WFS().readFeatures(response),
-                featureByYear = await dispatch("findBrwFeatureByYear", {features, year});
+                featureByYear = await helpers.findBrwFeatureByYear({features, year});
 
             commit("setBrwFeatures", features);
 
@@ -354,39 +353,14 @@ const actions = {
         }
     },
     /**
-     * find out if there is a brw feature for the given year and retruns it
-     * if not returns undefined
-     * @param {ol.Feature[]} features - list of all available brw features
-     * @param {string} year - the selected year
-     * @return {ol.Feature|undefined} brw feature
-     */
-    findBrwFeatureByYear ({commit}, {features, year}) {
-        const brwFeatures = Object.values(features);
-
-        return brwFeatures.find((feature) => {
-            return feature.get("jahrgang") === year;
-        });
-    },
-
-    /**
      * get the actually selected date and set date and feature to get extended feature attributes
      * @param {ol.Feature[]} feature - selected feature
      * @returns {void}
      */
-    async combineFeatureWithSelectedDate ({dispatch}, feature) {
-        const date = await dispatch("getDateByActiveLayerName");
+    async combineFeatureWithSelectedDate ({dispatch, getters}, feature) {
+        const date = await getters.getDateBySelectedLayerName;
 
         dispatch("extendFeatureAttributes", {feature, date});
-    },
-    /**
-     * Sends a request to convert the BRW
-     * @returns {void}
-     */
-    sendWpsConvertRequest ({state}) {
-        const data = helpers.convert({brw: state.selectedBrwFeature});
-
-        WPS.wpsRequest(state.wpsId, state.fmwProcess, data, helpers.handleConvertResponse);
-
     },
     /**
      * Sets the name of the active layer as date
@@ -402,37 +376,10 @@ const actions = {
         }
         return date;
     },
-    extendFeatureAttributes ({dispatch, commit}, {feature, date}) {
-        const isDMTime = parseInt(feature.get("jahrgang"), 10) < 2002;
-        let sw = helpers.getSW(feature);
+    extendFeatureAttributes ({commit, state}, {feature, date}) {
+        const isDMTime = parseInt(feature.get("jahrgang"), 10) < 2002,
+            sw = helpers.parseSW({feature});
 
-        if (sw) {
-
-            if (typeof sw === "string") {
-                sw = JSON.parse(sw);
-            }
-            else if (typeof sw === "object" && sw.normschichtwert_wohnen) {
-                sw.normschichtwert_wohnen = sw.normschichtwert_wohnen.replace(".", "").replace(",", ".");
-            }
-            if (sw.normschichtwert_wohnen) {
-                sw.normschichtwert_wohnenDM = isDMTime ? thousandsSeparator((parseFloat(sw.normschichtwert_wohnen, 10) * 1.95583).toFixed(1)) : "";
-                sw.normschichtwert_wohnen = thousandsSeparator(sw.normschichtwert_wohnen);
-            }
-            if (sw.normschichtwert_buero) {
-                sw.normschichtwert_bueroDM = isDMTime ? thousandsSeparator((parseFloat(sw.normschichtwert_buero, 10) * 1.95583).toFixed(1)) : "";
-                sw.normschichtwert_buero = thousandsSeparator(sw.normschichtwert_buero);
-            }
-            if (sw.normschichtwert_laden) {
-                sw.normschichtwert_ladenDM = isDMTime ? thousandsSeparator((parseFloat(sw.normschichtwert_laden, 10) * 1.95583).toFixed(1)) : "";
-                sw.normschichtwert_laden = thousandsSeparator(sw.normschichtwert_laden);
-            }
-            if (sw.schichtwerte) {
-                sw.schichtwerte.forEach(function (gfs) {
-                    gfs.schichtwertDM = isDMTime ? thousandsSeparator((parseFloat(gfs.schichtwert, 10) * 1.95583).toFixed(1)) : "";
-                    gfs.schichtwert = thousandsSeparator(gfs.schichtwert);
-                });
-            }
-        }
 
         feature.setProperties({
             "richtwert_dm": isDMTime ? thousandsSeparator(parseFloat(feature.get("richtwert_dm"), 10).toFixed(1)) : "",
@@ -451,7 +398,7 @@ const actions = {
         });
 
         commit("setSelectedBrwFeature", feature);
-        dispatch("sendWpsConvertRequest");
+        helpers.sendWpsConvertRequest({state});
         return feature;
     },
     /**
@@ -501,6 +448,7 @@ const actions = {
         commit("setSelectedBrwFeature", feature);
         commit("setConvertedBrw", state.selectedBrwFeature.get("convertedBrw"));
     }
+
 };
 
 export default actions;
