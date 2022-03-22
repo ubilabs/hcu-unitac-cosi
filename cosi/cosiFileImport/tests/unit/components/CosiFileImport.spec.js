4d888f3e48814f6f5ba882b4346f876d25ea9001
Dennis Rupnow <dennis.rupnow@outlook.com>
2022-03-22 13:49:16 +0100

    fixes cosiFileImport tests

--- cosi/cosiFileImport/tests/unit/components/CosiFileImport.spec.js
+++ cosi/cosiFileImport/tests/unit/components/CosiFileImport.spec.js
@@ -3,12 +3,33 @@ import {config, shallowMount, createLocalVue} from "@vue/test-utils";
 import CosiFileImportComponent from "../../../components/CosiFileImport.vue";
 import CosiFileImport from "../../../store/indexCosiFileImport";
 import {expect} from "chai";
+import sinon from "sinon";
+import * as crs from "masterportalAPI/src/crs";
+import Vuetify from "vuetify";
+import Vue from "vue";
+
+Vue.use(Vuetify);
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 
 config.mocks.$t = key => key;
+
+before(() => {
+    const namedProjections = [
+        ["EPSG:31467", "+title=Bessel/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+        ["EPSG:8395", "+title=ETRS89/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +datum=GRS80 +units=m +no_defs"],
+        ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]
+    ];
+
+    crs.registerProjections(namedProjections);
+    i18next.init({
+        lng: "cimode",
+        debug: false
+    });
+})
 
 describe("addons/cosiFileImport/components/CosiFileImport.vue", () => {
     const
@@ -39,6 +60,12 @@ describe("addons/cosiFileImport/components/CosiFileImport.vue", () => {
                     namespaced: true,
                     modules: {
                         CosiFileImport
+                    }
+                },
+                Language: {
+                    namespaced: true,
+                    getters: {
+                        currentLocale: () => sinon.stub()
                     }
                 }
             },
