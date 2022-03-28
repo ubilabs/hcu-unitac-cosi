ed6270fa5b2def9c41ca0115573756df1687f62a
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2022-03-28 17:25:44 +0200

    remove IMportedFeatureHandler

--- cosi/ImportedFeatureHandler/store/gettersImportedFeatureHandler.js
+++
@@ -1,9 +0,0 @@
-
-import {generateSimpleGetters} from "../../../../src/app-store/utils/generators";
-import ImportedFeatureHandlerState from "./stateImportedFeatureHandler";
-
-const getters = {
-    ...generateSimpleGetters(ImportedFeatureHandlerState)
-};
-
-export default getters;
