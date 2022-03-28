f939cc0d78fedf2def39263ec0e1927ad93fd639
Pascal Röhling <pascal.roehling@dataport.de>
2022-03-28 12:25:24 +0200

    Fix linting issues

--- gfiThemes/trafficCount/components/TrafficCountCalendarButtonGroup.vue
+++ gfiThemes/trafficCount/components/TrafficCountCalendarButtonGroup.vue
@@ -15,23 +15,23 @@ export default {
             document.getElementById(this.idPrefix + this.idSuffix).focus();
         }
     }
-}
+};
 </script>
 
 <template>
-     <span class="input-group-btn">
-         <button
-             :id="`${idPrefix + idSuffix}Button`"
-             class="btn btn-outline-dark"
-             type="button"
-             @click="openCalendar"
-         >
-             <span
-                 class="bootstrap-icon"
-                 aria-hidden="true"
-             >
-                 <i class="bi-grid-3x3-gap-fill" />
-             </span>
-         </button>
+    <span class="input-group-btn">
+        <button
+            :id="`${idPrefix + idSuffix}Button`"
+            class="btn btn-outline-dark"
+            type="button"
+            @click="openCalendar"
+        >
+            <span
+                class="bootstrap-icon"
+                aria-hidden="true"
+            >
+                <i class="bi-grid-3x3-gap-fill" />
+            </span>
+        </button>
     </span>
 </template>
