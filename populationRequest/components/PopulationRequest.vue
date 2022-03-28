43a1db063ce627726b2daed8bde80d51ff6830ba
Pascal Röhling <pascal.roehling@dataport.de>
2022-03-28 18:29:22 +0200

    Update template to look like the template prior to the bootstrap upgrade

--- populationRequest/components/PopulationRequest.vue
+++ populationRequest/components/PopulationRequest.vue
@@ -448,10 +448,10 @@ export default {
                 <div class="mb-3">
                     {{ translate("additional:modules.tools.populationRequest.select.info") }}
                 </div>
-                <div class="row graphicalSelectionContainer">
+                <div class="row">
                     <label
                         for="graphicalSelection"
-                        class="col-sm-5"
+                        class="col-sm-5 col-form-label"
                     >
                         {{ translate("additional:modules.tools.populationRequest.select.action") }}
                     </label>
