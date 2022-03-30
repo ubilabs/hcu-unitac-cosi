3f267ced0dbf7bf38032b58291fe9fe50b51ca61
Pascal Röhling <pascal.roehling@dataport.de>
2022-03-30 17:12:55 +0200

    Fix various alignment and style issues

--- populationRequest/components/PopulationRequest.vue
+++ populationRequest/components/PopulationRequest.vue
@@ -448,10 +448,10 @@ export default {
                 <div class="mb-3">
                     {{ translate("additional:modules.tools.populationRequest.select.info") }}
                 </div>
-                <div class="row">
+                <div class="graphicalSelectionContainer">
                     <label
                         for="graphicalSelection"
-                        class="col-sm-5 col-form-label"
+                        class="col-sm-5"
                     >
                         {{ translate("additional:modules.tools.populationRequest.select.action") }}
                     </label>
