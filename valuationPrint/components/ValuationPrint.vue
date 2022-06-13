98a0d4601adc4b7f73fa6f3bf59831f3e8ce3375
PinkUndDerBrain <sebastian.duden@gv.hamburg.de>
2022-06-13 15:57:29 +0200

    G31DEV1-743 G31DEV1-741 update valuationPrint. Now it is possible to select parcel features and remove them again.

--- valuationPrint/components/ValuationPrint.vue
+++ valuationPrint/components/ValuationPrint.vue
@@ -1,8 +1,13 @@
 <script>
-import ToolTemplate from "../../../src/modules/tools/ToolTemplate.vue";
-import {mapMutations, mapGetters} from "vuex";
-import mutations from "../store/mutationsValuationPrint";
+import {Fill, Stroke, Style} from "ol/style";
+import getComponent from "../../../src/utils/getComponent";
 import getters from "../store/gettersValuationPrint";
+import Feature from "ol/Feature";
+import {mapActions, mapGetters, mapMutations} from "vuex";
+import mutations from "../store/mutationsValuationPrint";
+import {Select} from "ol/interaction";
+import {singleClick} from "ol/events/condition";
+import ToolTemplate from "../../../src/modules/tools/ToolTemplate.vue";
 
 export default {
     name: "ValuationPrint",
@@ -12,13 +17,91 @@ export default {
     computed: {
         ...mapGetters("Tools/ValuationPrint", Object.keys(getters))
     },
+    watch: {
+        /**
+         * Activates the select interaction if the tool is active, ohterwise it is deactivated.
+         * @param {Boolean} newValue - If the tool is active or not.
+         * @returns {void}
+         */
+        active (newValue) {
+            this.select.setActive(newValue);
+        }
+    },
     created () {
+        this.setSelectInteraction();
+
         this.$on("close", () => {
             this.setActive(false);
+            // The value "isActive" of the Backbone model is also set to false to change the CSS class in the menu (menu/desktop/tool/view.toggleIsActiveClass)
+            const model = getComponent(this.id);
+
+            if (model) {
+                model.set("isActive", false);
+            }
         });
     },
     methods: {
-        ...mapMutations("Tools/ValuationPrint", Object.keys(mutations))
+        ...mapMutations("Tools/ValuationPrint", Object.keys(mutations)),
+        ...mapActions("Maps", ["addInteraction"]),
+
+        /**
+         * Removes the passed feature from the collection where the select interaction will place the selected features.
+         * @param {ol/Feature} feature - The feature to be removed.
+         * @returns {void}
+         */
+        removeFeature (feature) {
+            if (feature instanceof Feature) {
+                this.select.getFeatures().remove(feature);
+            }
+        },
+
+        /**
+         * Sets the select interaction (non-reactive state), adds it a "change:active" listener and adds it to the map.
+         * @returns {void}
+         */
+        setSelectInteraction () {
+            this.select = new Select({
+                layers: (layer) => layer.get("id") === "6076",
+                style: new Style({
+                    fill: new Fill({
+                        color: "rgba(255,255,255,0)"
+                    }),
+                    stroke: new Stroke({
+                        color: "#de2d26",
+                        width: 5
+                    })
+                }),
+                addCondition: singleClick,
+                removeCondition: singleClick
+            });
+
+            this.select.on("change:active", this.styleSelectedFeatures);
+            this.addInteraction(this.select);
+        },
+
+        /**
+         * Sets the style of the selected features depending on the activity of the select interaction.
+         * If the interaction is active, all existing featurers are styled using the select interaction style.
+         * If it is not, the layer style is used.
+         * @param {ol/Object.ObjectEvent} evt - OpenLayers Object Event.
+         * @param {String} evt.key - The name of the property whose value is changing.
+         * @param {ol/interaction/Select} evt.target - The event target. In this case the select interaction.
+         * @returns {void}
+         */
+        styleSelectedFeatures ({key, target}) {
+            const features = target.getFeatures();
+
+            if (target.get(key)) {
+                features.forEach(feature => {
+                    feature.setStyle(target.getStyle());
+                });
+            }
+            else {
+                features.forEach(feature => {
+                    feature.setStyle(false);
+                });
+            }
+        }
     }
 };
 </script>
@@ -35,7 +118,37 @@ export default {
             v-if="active"
             #toolBody
         >
-            <div class="valuation-print" />
+            <div class="valuation-print">
+                <div
+                    v-for="feature in select.getFeatures().getArray()"
+                    :key="feature.get('flstnrzae')"
+                >
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            Flurstück:
+                        </li>
+                        <li class="list-inline-item">
+                            {{ feature.get("flstnrzae") }}
+                        </li>
+                        <li class="list-inline-item">
+                            Gemarkung:
+                        </li>
+                        <li class="list-inline-item">
+                            {{ feature.get("gemarkung") }}
+                        </li>
+                    </ul>
+                    <div class="mt-2">
+                        <button
+                            type="button"
+                            class="btn btn-primary btn-sm"
+                            @click="removeFeature(feature)"
+                        >
+                            Entfernen
+                        </button>
+                    </div>
+                    <hr>
+                </div>
+            </div>
         </template>
     </ToolTemplate>
 </template>
--- valuationPrint/tests/unit/components/ValuationPrint.spec.js
+++ valuationPrint/tests/unit/components/ValuationPrint.spec.js
@@ -1,29 +1,59 @@
-import {config, shallowMount} from "@vue/test-utils";
+import {config, shallowMount, createLocalVue} from "@vue/test-utils";
 import {expect} from "chai";
+import Feature from "ol/Feature";
+import {Select} from "ol/interaction";
+import sinon from "sinon";
 import ValuationPrint from "../../../components/ValuationPrint.vue";
+import Vuex from "vuex";
+
 config.mocks.$t = key => key;
 
-describe("addons/valuation/components/ValuationPrint.vue", () => {
+const localVue = createLocalVue();
 
-    const factory = {
-        getShallowMount: (values = {}, isActive = false) => {
-            return shallowMount(ValuationPrint, {
-                data () {
-                    return {
-                        ...values
-                    };
-                },
-                computed: {
-                    name: () => "Hallo",
-                    renderToWindow: () => false,
-                    resizableWindow: () => true,
-                    deactivateGFI: () => true,
-                    active: () => isActive,
-                    icon: () => "bi-bar-chart-line-fill"
+localVue.use(Vuex);
+
+describe("addons/valuation/components/ValuationPrint.vue", () => {
+    const features = [
+            new Feature({
+                flstnrzae: "12345",
+                gemarkung: "ueberall"
+            }),
+            new Feature({
+                flstnrzae: "67890",
+                gemarkung: "nirgends"
+            })
+        ],
+        mockMapActions = {
+            addInteraction: sinon.spy()
+        },
+        store = new Vuex.Store({
+            modules: {
+                Maps: {
+                    namespaced: true,
+                    actions: mockMapActions
                 }
-            });
-        }
-    };
+            }
+        }),
+        factory = {
+            getShallowMount: (values = {}, isActive = false) => {
+                return shallowMount(ValuationPrint, {
+                    data () {
+                        return {
+                            ...values
+                        };
+                    },
+                    computed: {
+                        active: () => isActive,
+                        name: () => "Hallo",
+                        icon: () => "small",
+                        renderToWindow: () => false,
+                        resizableWindow: () => false
+                    },
+                    store,
+                    localVue
+                });
+            }
+        };
 
     describe("Component DOM", () => {
         it("should exist", () => {
@@ -49,6 +79,134 @@ describe("addons/valuation/components/ValuationPrint.vue", () => {
             const wrapper = factory.getShallowMount({}, true);
 
             expect(wrapper.find(".valuation-print").exists()).to.be.true;
+        });
+
+        it("should find one list per feature", async () => {
+            const wrapper = factory.getShallowMount({}, true);
+
+            wrapper.vm.select.getFeatures().push(features[0]);
+            wrapper.vm.select.getFeatures().push(features[1]);
+            await wrapper.vm.$forceUpdate();
+
+            expect(wrapper.findAll(".list-inline")).to.be.lengthOf(2);
+        });
+
+        it("should render the feature properties correctly", async () => {
+            const wrapper = factory.getShallowMount({}, true);
+
+            wrapper.vm.select.getFeatures().push(features[0]);
+            await wrapper.vm.$forceUpdate();
+
+            expect(wrapper.findAll(".list-inline-item").at(0).text()).to.be.equal("Flurstück:");
+            expect(wrapper.findAll(".list-inline-item").at(1).text()).to.be.equal("12345");
+            expect(wrapper.findAll(".list-inline-item").at(2).text()).to.be.equal("Gemarkung:");
+            expect(wrapper.findAll(".list-inline-item").at(3).text()).to.be.equal("ueberall");
+        });
+
+        it("should find one remove button per feature", async () => {
+            const wrapper = factory.getShallowMount({}, true);
+
+            wrapper.vm.select.getFeatures().push(features[0]);
+            wrapper.vm.select.getFeatures().push(features[1]);
+            await wrapper.vm.$forceUpdate();
+
+            expect(wrapper.findAll("button")).to.be.lengthOf(2);
+        });
+    });
+
+    describe("User Interactions", () => {
+        it("should call removeFeature if user click the remove button", async () => {
+            const spyRemoveFeature = sinon.spy(ValuationPrint.methods, "removeFeature"),
+                wrapper = factory.getShallowMount({}, true);
+
+            wrapper.vm.select.getFeatures().push(features[0]);
+            wrapper.vm.select.getFeatures().push(features[1]);
+            await wrapper.vm.$forceUpdate();
+            await wrapper.findAll("button").at(1).trigger("click");
+
+            expect(spyRemoveFeature.calledOnce).to.be.true;
+
+            spyRemoveFeature.restore();
+        });
+    });
+
+    describe("Hooks", () => {
+        describe("Created", () => {
+            it("should call setSelectInteraction if the component is created", () => {
+                const spySetSelectInteraction = sinon.spy(ValuationPrint.methods, "setSelectInteraction");
+
+                factory.getShallowMount({});
+
+                expect(spySetSelectInteraction.calledOnce).to.be.true;
+
+                spySetSelectInteraction.restore();
+            });
+        });
+
+    });
+
+    describe("Methods", () => {
+        describe("styleSelectedFeatures", () => {
+            it("should style the features with the select interaction style if the interaction is active", () => {
+                const wrapper = factory.getShallowMount({});
+
+                wrapper.vm.select.getFeatures().push(features[0]);
+                wrapper.vm.styleSelectedFeatures({key: "active", target: wrapper.vm.select});
+                expect(wrapper.vm.select.getFeatures().getArray()[0].getStyle()).to.deep.equal(wrapper.vm.select.getStyle());
+
+            });
+
+            it("should style the features with the layer style if the interaction is not active", () => {
+                const wrapper = factory.getShallowMount({});
+
+                wrapper.vm.select.getFeatures().push(features[0]);
+                wrapper.vm.select.setActive(false);
+                wrapper.vm.styleSelectedFeatures({key: "active", target: wrapper.vm.select});
+                expect(wrapper.vm.select.getFeatures().getArray()[0].getStyle()).to.be.false;
+
+            });
+        });
+        describe("removeFeature", () => {
+            it("should remove a feature from the select interaction", () => {
+                const wrapper = factory.getShallowMount({}, true);
+
+                wrapper.vm.select.getFeatures().push(features[0]);
+                wrapper.vm.select.getFeatures().push(features[1]);
+                wrapper.vm.removeFeature(features[0]);
+
+                expect(wrapper.vm.select.getFeatures().getArray()).to.be.lengthOf(1);
+            });
+            it("should only remove data from type ol/Feature", () => {
+                const wrapper = factory.getShallowMount({}, true);
+
+                wrapper.vm.select.getFeatures().push(features[0]);
+                wrapper.vm.select.getFeatures().push(features[1]);
+                wrapper.vm.removeFeature(122);
+                wrapper.vm.removeFeature({});
+                wrapper.vm.removeFeature(true);
+                wrapper.vm.removeFeature(undefined);
+                wrapper.vm.removeFeature(null);
+                wrapper.vm.removeFeature([]);
+
+                expect(wrapper.vm.select.getFeatures().getArray()).to.be.lengthOf(2);
+            });
+        });
+        describe("setSelectInteraction", () => {
+            it("should set openlayers select interaction", () => {
+                const wrapper = factory.getShallowMount({}, true);
+
+                expect(wrapper.vm.select).to.be.exists;
+                expect(wrapper.vm.select instanceof Select).to.be.true;
+            });
+            it("should call addInteraction", () => {
+                const spyAddInteraction = sinon.spy(ValuationPrint.methods, "addInteraction");
+
+                factory.getShallowMount({}, true);
+
+                expect(spyAddInteraction.calledOnce).to.be.true;
+
+                spyAddInteraction.restore();
+            });
         });
     });
 });
