0c6f28bce15b47350f99258a3296cc2db801e4b0
Feng Feng <feng.feng@gv.hamburg.de>
2022-06-28 15:38:04 +0200

    G31DEV1-653 Refactor: Verkehrsfunction from backbone to vue

--- verkehrsfunctions/tests/unit/verkehrsfunctions.spec.js
+++ verkehrsfunctions/tests/unit/verkehrsfunctions.spec.js
@@ -1,22 +1,20 @@
 import {expect} from "chai";
-import Verkehrsfunctions from "@addons/verkehrsfunctions/verkehrsfunctions.js";
+import {
+    getAbsTrafficCount,
+    getKfzTrafficCount,
+    getPhenomenonTimeRange
+} from "../../verkehrsfunctions";
 import * as moment from "moment";
 
-describe("ADDON: Verkehrsfunctions", function () {
-    let model;
-
-    before(function () {
-        model = new Verkehrsfunctions();
-    });
-
-    describe("getPhenomenonTimeRange", function () {
+describe("ADDON: Verkehrsfunctions", () => {
+    describe("getPhenomenonTimeRange", () => {
         it("should parse the right time format", function () {
             const startTime = "2020-03-26T18:45:01.000Z",
                 endTime = "2020-03-26T19:00:00.000Z",
                 expectedStart = moment(startTime).format("DD.MM.YYYY, HH:mm"),
                 expectedEnd = moment(endTime).format("HH:mm");
 
-            expect(model.getPhenomenonTimeRange(startTime, endTime)).to.equal(expectedStart + " Uhr - " + expectedEnd + " Uhr");
+            expect(getPhenomenonTimeRange(startTime, endTime)).to.equal(expectedStart + " Uhr - " + expectedEnd + " Uhr");
         });
     });
 
@@ -24,13 +22,13 @@ describe("ADDON: Verkehrsfunctions", function () {
         it("should return text with 'No data'", function () {
             const dataStreamValue = null;
 
-            expect(model.getAbsTrafficCount(dataStreamValue)).to.equal("No data");
+            expect(getAbsTrafficCount(dataStreamValue)).to.equal("No data");
         });
 
         it("should parse the right the absolute count", function () {
             const dataStreamValue = "219";
 
-            expect(model.getAbsTrafficCount(dataStreamValue)).to.equal("219");
+            expect(getAbsTrafficCount(dataStreamValue)).to.equal("219");
         });
     });
 
@@ -42,18 +40,18 @@ describe("ADDON: Verkehrsfunctions", function () {
 
         it("should return a value for Kfz", function () {
             type = "Anzahl_Kfz";
-            expect(model.getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("219");
+            expect(getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("219");
         });
 
         it("should return a value for SV", function () {
             type = "Anzahl_SV";
-            expect(model.getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("35");
+            expect(getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("35");
         });
 
         it("should return a NO data", function () {
             layerName = "Anzahl_Rad_Zaehlstelle_15-Min | Anzahl_SV_Zaehlstelle_15-Min";
             type = "Anzahl_Kfz";
-            expect(model.getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("No data");
+            expect(getKfzTrafficCount(absTrafficCount, layerName, type)).to.equal("No data");
         });
     });
 });
