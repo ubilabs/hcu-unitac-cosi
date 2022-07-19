9eb52253984eab0a7708b6ba198c0a01bcb8d32e
fmlgv <friedrich.mueller@gv.hamburg.de>
2022-07-19 17:17:35 +0200

    BG-2602: Add component tests

--- refugeeHomes/tests/unit/components/RefugeeHomes.spec.js
+++ refugeeHomes/tests/unit/components/RefugeeHomes.spec.js
@@ -1,20 +1,11 @@
 import Vuex from "vuex";
-import {config, shallowMount, mount, createLocalVue} from "@vue/test-utils";
+import {config, shallowMount, createLocalVue} from "@vue/test-utils";
 import RefugeeHomesComponent from "../../../components/RefugeeHomes.vue";
 import RefugeeHomes from "../../../store/indexRefugeeHomes";
-import RefugeeHomesState from "../../../store/stateRefugeeHomes";
 import {expect} from "chai";
 import sinon from "sinon";
 
-const localVue = createLocalVue(),
-    map = {
-        id: "ol",
-        mode: "2D",
-        updateSize: sinon.spy()
-    };
-
-mapCollection.clear();
-mapCollection.addMap(map, "2D");
+const localVue = createLocalVue();
 
 localVue.use(Vuex);
 config.mocks.$t = key => key;
@@ -26,8 +17,8 @@ describe("ADDONS: addons/refugeeHomes/components/RefugeeHomes.vue", () => {
                 tools: {
                     children: {
                         refugeeHomes: {
-                            name: "translate#additional:menu.tools.refugeehomes",
-                            glyphicon: "glyphicon-share"
+                            "icon": "house-fill",
+                            "name": "refugeehomes"
                         }
                     }
                 }
@@ -37,43 +28,44 @@ describe("ADDONS: addons/refugeeHomes/components/RefugeeHomes.vue", () => {
     let store, wrapper;
 
     beforeEach(() => {
-
-
         store = new Vuex.Store({
             namespaces: true,
             modules: {
                 Tools: {
                     namespaced: true,
                     modules: {
-                        RefugeeHomes: {
-                            filteredFeatures: [{0: {bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}, 1: {}}],
-                            features: [{bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}],
-                            active: true,
-                            icon: "house-fill",
-                            id: "refugeehomes"
-                        }
+                        RefugeeHomes
                     }
                 },
                 Maps: {
                     namespaced: true,
+                    resetView: sinon.spy(),
+                    actions: {
+                        resetView: sinon.spy()
+                    },
                     getters: {
                         clickCoordinate: () => [100, 200]
                     }
                 },
                 MapMarker: {
                     namespaced: true,
+                    actions: {
+                        removePointMarker: sinon.spy()
+                    },
                     getters: {
                         markerPoint: () => sinon.spy()
                     }
-                },
-                state: {
-                    configJson: mockConfigJson,
-                    features: [{bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}],
-                    filteredFeatures: [{0: {bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}, 1: {}}],
-                    active: false
                 }
+            },
+            state: {
+                configJson: mockConfigJson,
+                features: [{bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}],
+                filteredFeatures: [{0: {bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}, 1: {}}]
             }
         });
+        store.commit("Tools/RefugeeHomes/addFeature", [{bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}]);
+        store.commit("Tools/RefugeeHomes/addFilteredFeature", [{0: {bezirk: "Altona", bezeichnung: "123", strasse: "musterstr", stadtteil: "5", platzzahl: 130}, 1: {}}]);
+        store.commit("Tools/RefugeeHomes/setActive", true);
     });
     afterEach(function () {
         sinon.restore();
@@ -84,15 +76,36 @@ describe("ADDONS: addons/refugeeHomes/components/RefugeeHomes.vue", () => {
     });
 
     it("renders the RefugeeHomes", () => {
-        store.Tools.RefugeeHomes.active = true;
-        // store.commit("Tools/RefugeeHomes/setActive", true);
         wrapper = shallowMount(RefugeeHomesComponent, {store, localVue});
+
         expect(wrapper.find("#refugeehomes").exists()).to.be.true;
+        expect(wrapper.find("tr").exists()).to.be.true;
     });
 
     it("do not render the RefugeeHomes if not active", () => {
         store.commit("Tools/RefugeeHomes/setActive", false);
         wrapper = shallowMount(RefugeeHomesComponent, {store, localVue});
+
         expect(wrapper.find("#refugeehomes").exists()).to.be.false;
+    });
+    it("if all features watcher is true the sortFeatures is executed", async () => {
+        const sortFeaturesStub = sinon.stub();
+
+        wrapper = shallowMount(RefugeeHomesComponent, {store, localVue});
+        wrapper.setMethods({sortFeatures: sortFeaturesStub});
+        wrapper.vm.allFeatures = true;
+        await wrapper.vm.$nextTick();
+        expect(sortFeaturesStub.calledOnce).to.be.true;
+    });
+    it("if the tool is closed the highlighted layer are removed", async () => {
+        const removeHighlightLayerStub = sinon.stub();
+
+        wrapper = shallowMount(RefugeeHomesComponent, {store, localVue});
+        wrapper.setMethods({removeHighlightLayer: removeHighlightLayerStub});
+        wrapper.vm.close();
+
+        await wrapper.vm.$nextTick();
+        expect(removeHighlightLayerStub.called).to.be.true;
+
     });
 });
