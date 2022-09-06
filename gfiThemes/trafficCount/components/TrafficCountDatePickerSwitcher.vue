c79523f2d8f732520c7f1b879100c3e13c3ffae0
PinkUndDerBrain <sebastian.duden@gv.hamburg.de>
2022-09-06 12:21:49 +0200

    G31DEV1-1152-TrafficCount: Add datepicker switcher.

---
+++ gfiThemes/trafficCount/components/TrafficCountDatePickerSwitcher.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+    name: "TrafficCountDatePickerSwitcher",
+    props: {
+        year: {
+            type: String,
+            required: true
+        },
+        month: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        /**
+         * Emits the "yearDown" event.
+         * @returns {void}
+         */
+        yearDown () {
+            this.$emit("yearDown");
+        },
+
+        /**
+         * Emits the "yearUp" event.
+         * @returns {void}
+         */
+        yearUp () {
+            this.$emit("yearUp");
+        },
+
+        /**
+         * Emits the "monthDown" event.
+         * @returns {void}
+         */
+        monthDown () {
+            this.$emit("monthDown");
+        },
+
+        /**
+         * Emits the "monthUp" event.
+         * @returns {void}
+         */
+        monthUp () {
+            this.$emit("monthUp");
+        }
+    }
+};
+</script>
+
+<template>
+    <div
+        id="date-picker-switcher"
+        class="text-center py-2"
+    >
+        <button
+            type="button"
+            class="btn btn-light btn-sm float-start"
+            @click="yearDown"
+        >
+            <i class="bi bi-chevron-double-left" />
+        </button>
+        <button
+            type="button"
+            class="btn btn-light btn-sm float-start"
+            @click="monthDown"
+        >
+            <i class="bi bi-chevron-left" />
+        </button>
+        <button
+            type="button"
+            class="btn btn-light btn-sm float-end"
+            @click="yearUp"
+        >
+            <i class="bi bi-chevron-double-right" />
+        </button>
+        <button
+            type="button"
+            class="btn btn-light btn-sm float-end"
+            @click="monthUp"
+        >
+            <i class="bi bi-chevron-right" />
+        </button>
+        <span class="date-label">{{ month }} {{ year }}</span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    @import "~variables";
+
+    #date-picker-switcher {
+        button {
+            border: 0;
+            box-shadow: none;
+            background: none;
+            &:hover {
+                color: $blue;
+            }
+        }
+        .date-label {
+            font-family: $font_family_default;
+            font-size: $font_size_big;
+        }
+    }
+</style>
