af22d07f0ff42090718c16f2d92d4e13f0fdbf0e
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-01-27 12:06:45 +0100

    BG-3715: make namespace dynamic

--- refugeeHomes/store/actionsRefugeeHomes.js
+++ refugeeHomes/store/actionsRefugeeHomes.js
@@ -1,5 +1,21 @@
-import rawLayerList from "@masterportal/masterportalapi/src/rawLayerList";
+import {getLayerWhere} from "@masterportal/masterportalapi/src/rawLayerList";
 import axios from "axios";
+
+/**
+     * Help function to define the namespace
+     * @param {String} str String to search within
+     * @param {String} start Start tag for search
+     * @param {String} end End tag for search
+     * @returns {void}
+     */
+function getStringBetween (str, start, end) {
+    const result = str.match(new RegExp(start + "(.*)" + end));
+
+    if (result === null) {
+        return "app";
+    }
+    return result[1];
+}
 
 const actions = {
     /**
@@ -10,7 +26,7 @@ const actions = {
      */
     requestRawLayerList ({getters, dispatch}) {
         getters.layerIds.forEach(async (layerId) => {
-            const rawLayer = rawLayerList.getLayerWhere({id: layerId}),
+            const rawLayer = getLayerWhere({id: layerId}),
                 getFeatureUrl = await dispatch("buildAndGetRequestUrl", rawLayer);
 
             await dispatch("sendRequest", getFeatureUrl);
@@ -91,7 +107,8 @@ const actions = {
      */
     parseFeatures ({getters, dispatch, commit, rootGetters}, data) {
         const xmlData = new DOMParser().parseFromString(data, "text/xml"),
-            hits = xmlData.getElementsByTagName("wfs:member");
+            hits = xmlData.getElementsByTagName("wfs:member"),
+            nameSpace = getStringBetween(data, "</", ":bezeichnung");
 
         let feature,
             element,
@@ -104,11 +121,7 @@ const actions = {
             };
 
             getters.featureAttributes.forEach((attr) => {
-                element = hit.getElementsByTagName("app:" + attr)[0];
-
-                if (typeof element === "undefined") {
-                    element = hit.getElementsByTagName("de.hh.up:" + attr)[0];
-                }
+                element = hit.getElementsByTagName(nameSpace + ":" + attr)[0];
 
                 if (typeof element !== "undefined") {
                     if (attr === "pfad") {
