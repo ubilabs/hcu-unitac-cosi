f1c31484014176815994fbbff1c722259bcf9d49
PinkUndDerBrain <sebastian.duden@gv.hamburg.de>
2023-03-31 11:49:06 +0200

    G31DEV1-1433-Theme-Datatable-Add Table can be sorted now.

--- gfiThemes/dataTable/components/DataTable.vue
+++ gfiThemes/dataTable/components/DataTable.vue
@@ -4,6 +4,7 @@ import {mapGetters} from "vuex";
 import getters from "../../../../src/modules/tools/gfi/store/gettersGfi";
 import {isWebLink} from "../../../../src/utils/urlHelper.js";
 import ExportButtonCSV from "../../../../src/share-components/exportButton/components/ExportButtonCSV.vue";
+import sortBy from "../../../../src/utils/sortBy";
 
 export default {
     name: "DataTable",
@@ -16,39 +17,129 @@ export default {
             required: true
         }
     },
+    data () {
+        return {
+            columns: this.getColumns(this.feature.getAttributesToShow()),
+            rows: this.feature.getFeatures().map(singleFeature => singleFeature.getMappedProperties())
+        };
+    },
     computed: {
-        refinedData: function () {
-            const result = {
-                head: [],
-                rows: []
-            };
-
-            this.feature.getFeatures().forEach(singleFeature => {
-
-                const mappedProps = singleFeature.getMappedProperties();
-
-                Object.keys(mappedProps).forEach(attrKey => {
-                    if (mappedProps[attrKey] !== undefined && typeof mappedProps[attrKey] === "string" && result.head.indexOf(attrKey) === -1) {
-                        result.head.push(attrKey);
-                    }
-                });
-
-                result.rows.push(mappedProps);
-            });
-
-            return result;
+        /**
+         * Gets the unsorted and unfiltered rows.
+         * @returns {Object[]} The origin rows.
+         */
+        originRows: function () {
+            return this.feature.getFeatures().map(singleFeature => singleFeature.getMappedProperties());
         },
+
         enableDownload: function () {
             return this.feature?.getTheme()?.params?.enableDownload;
+        },
+
+        /**
+         * Returns whether the table is sortable.
+         * @returns {Boolean} True if the table is sortable otherwise false.
+         */
+        isSortable: function () {
+            return this.feature.getTheme()?.params?.isSortable || false;
         }
     },
     created () {
-        this.downloadData = this.refinedData?.rows;
         this.fileName = this.feature?.getTitle();
     },
     methods: {
         ...mapGetters("Tools/Gfi", Object.keys(getters)),
-        isWebLink
+        isWebLink,
+
+        /**
+         * Creates and returns the columns for the table.
+         * @param {Object} gfiAttributes - The attributes to be displayed.
+         * @returns {Object[]} The column objects.
+         */
+        getColumns (gfiAttributes) {
+            return Object.keys(gfiAttributes).map((key, idx) => {
+                return {
+                    name: gfiAttributes[key],
+                    order: "origin",
+                    index: idx
+                };
+            });
+        },
+
+        /**
+         * Gets a specific icon class to the passed order.
+         * @param {String} order - The order in which the table is sorted.
+         * @returns {String} The icon css class for the given order.
+         */
+        getIconClassByOrder (order) {
+            if (order === "asc") {
+                return "bi-arrow-up";
+            }
+            if (order === "desc") {
+                return "bi-arrow-down";
+            }
+            return "bi-arrow-down-up origin-order";
+        },
+
+        /**
+         * Gets the old sorted column if another one is to be sorted.
+         * @param {Object[]} columns - The columns objects.
+         * @param {Number} indexNewColumn - The index of the column that will be sorted.
+         * @returns {Object|undefined} The old sorted column or undefined if it not exits.
+         */
+        getOldSortedColumn (columns, indexNewColumn) {
+            return columns.find(col => {
+                return col.order !== "origin" && col.index !== indexNewColumn;
+            });
+        },
+
+        /**
+         * Gets the rows sorted in the correct order.
+         * @param {Object[]} rows - The rows.
+         * @param {String} order - The order in which the table is sorted.
+         * @param {String} columnName - The name of the column to sort by.
+         * @returns {Object[]} The sorted rows.
+         */
+        getSortedRows (rows, order, columnName) {
+            if (order === "asc") {
+                return sortBy(rows, columnName);
+            }
+            if (order === "desc") {
+                return sortBy(rows, columnName).reverse();
+            }
+            return this.originRows;
+        },
+
+        /**
+         * Gets the next sort order.
+         * @param {String} order - The order in which the table is sorted.
+         * @returns {String} The sort order.
+         */
+        getSortOrder (order) {
+            if (order === "origin") {
+                return "asc";
+            }
+            if (order === "asc") {
+                return "desc";
+            }
+            return "origin";
+        },
+
+        /**
+         * Sets the order and sorts the table by the given column.
+         * Sorting by a new column resets the order of the old column.
+         * @param {Object} column - The column to sort by.
+         * @returns {void}
+         */
+        runSorting (column) {
+            const oldColumn = this.getOldSortedColumn(this.columns, column.index);
+
+            if (oldColumn) {
+                oldColumn.order = "origin";
+            }
+            column.order = this.getSortOrder(column.order);
+            this.rows = this.getSortedRows(this.rows, column.order, column.name);
+        }
     }
 };
 </script>
@@ -56,36 +147,43 @@ export default {
 <template>
     <div id="table-data-container">
         <table
-            v-if="refinedData.rows.length > 0"
+            v-if="rows.length > 0"
             class="table table-hover"
         >
             <thead>
                 <th
-                    v-for="propNameKey in refinedData.head"
-                    :key="propNameKey"
+                    v-for="col in columns"
+                    :key="col.index"
                 >
-                    {{ propNameKey }}
+                    {{ col.name }}
+                    <div
+                        v-if="isSortable"
+                        class="bootstrap-icon"
+                        :class="getIconClassByOrder(col.order)"
+                        @click="runSorting(col)"
+                        @keypress="runSorting(col)"
+                    />
                 </th>
             </thead>
 
             <tbody>
                 <tr
-                    v-for="(singleRow, index1) in refinedData.rows"
+                    v-for="(singleRow, index1) in rows"
                     :key="index1"
                 >
                     <td
-                        v-for="(propKey, index2) in refinedData.head"
+                        v-for="(col, index2) in columns"
                         :key="index2"
                     >
                         <a
-                            v-if="isWebLink(singleRow[propKey])"
-                            :href="singleRow[propKey]"
+                            v-if="isWebLink(singleRow[col.name])"
+                            :href="singleRow[col.name]"
                             target="_blank"
                         >
-                            {{ singleRow[propKey] ? singleRow[propKey] : '' }}
+                            {{ singleRow[col.name] ? singleRow[col.name] : '' }}
                         </a>
                         <span v-else>
-                            {{ singleRow[propKey] ? singleRow[propKey] : '' }}
+                            {{ singleRow[col.name] ? singleRow[col.name] : '' }}
                         </span>
                     </td>
                 </tr>
@@ -98,7 +196,7 @@ export default {
             <ExportButtonCSV
                 :url="false"
                 :filename="fileName"
-                :data="downloadData"
+                :data="rows"
                 :use-semicolon="true"
                 :title="$t('modules.tools.filter.download.labelBtn')"
             />
@@ -112,12 +210,22 @@ export default {
 #table-data-container {
     margin:6px 15px 0 12px;
 
+    .origin-order {
+        opacity: 0.3;
+        &:hover {
+            opacity: 1;
+        }
+    }
+
     table {
         margin: 0;
         th {
             position: sticky;
             top: 0px;
             background: $white;
+            div {
+                cursor: pointer;
+            }
         }
         td, th {
             padding: 6px;
--- gfiThemes/dataTable/tests/unit/components/DataTable.spec.js
+++ gfiThemes/dataTable/tests/unit/components/DataTable.spec.js
@@ -1,89 +1,84 @@
 import Vuex from "vuex";
-import {shallowMount, createLocalVue} from "@vue/test-utils";
+import {shallowMount, createLocalVue, config} from "@vue/test-utils";
 import {expect} from "chai";
 import DataTableTheme from "../../../components/DataTable.vue";
+import sinon from "sinon";
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 
+config.mocks.$t = key => key;
+
 describe("/src/modules/tools/gfi/components/themes/dataTable/components/DataTable.vue", () => {
     let wrapper,
-        featureData = {
-            getTheme: () => {
-                return {
-                    "name": "DataTable",
-                    "params": {
-                        "enableDownload": true
-                    }
-                };
-            },
-            getTitle: () => "DataTable",
-            getAttributesToShow: () => {
-                return {};
-            },
-            getMimeType: () => "text/xml",
-            getFeatures: () => {
-                return [{
-                    getMappedProperties: () => {
-                        return {
-                            "Entnahme Datum": "2019",
-                            "OHG in Meter": "0.10",
-                            "UHG in Meter": "0.35",
-                            "Arsen": "15,9",
-                            "Cadmium": "1,38",
-                            "Chrom": "21,6",
-                            "Kupfer": "290,0",
-                            "Quecksilber": "0,285",
-                            "Nickel": "24,9",
-                            "Blei": "289,0",
-                            "Thallium": "---",
-                            "Zink": "393,0",
-                            "Molybdän": "4,53",
-                            "Einheit": "mg/kg TM"
-                        };
-                    }
-                },
-                {
-                    getMappedProperties: () => {
-                        return {
-                            "Entnahme Datum": "2019",
-                            "OHG in Meter": "0.00",
-                            "UHG in Meter": "0.10",
-                            "Arsen": "14,7",
-                            "Cadmium": "1,34",
-                            "Chrom": "40,5",
-                            "Kupfer": "774,0",
-                            "Quecksilber": "0,346",
-                            "Nickel": "22,9",
-                            "Blei": "209,0",
-                            "Thallium": "---",
-                            "Zink": "568,0",
-                            "Molybdän": "19,8",
-                            "Einheit": "mg/kg TM"
-                        };
-                    }
-                }];
-            }
-        };
+        spyRunSorting;
 
-    const mockGetters = {
-            gfiFeatures: () => featureData
-        },
-
-        store = new Vuex.Store({
-            namespaces: true,
-            modules: {
-                Map: {
-                    namespaced: true,
-                    getters: mockGetters
+    const featureData = {
+        getTheme: () => {
+            return {
+                "name": "DataTable",
+                "params": {
+                    "enableDownload": true,
+                    "isSortable": true
                 }
-            }
-        });
+            };
+        },
+        getTitle: () => "DataTable",
+        getAttributesToShow: () => {
+            return {
+                entnahme_datum: "Entnahme Datum",
+                ohg_in_meter: "OHG in Meter"
+            };
+        },
+        getMimeType: () => "text/xml",
+        getFeatures: () => {
+            return [{
+                getMappedProperties: () => {
+                    return {
+                        "Entnahme Datum": "2019",
+                        "OHG in Meter": "0.10",
+                        "UHG in Meter": "0.35",
+                        "Arsen": "15,9",
+                        "Cadmium": "1,38",
+                        "Chrom": "21,6",
+                        "Kupfer": "290,0",
+                        "Quecksilber": "0,285",
+                        "Nickel": "24,9",
+                        "Blei": "289,0",
+                        "Thallium": "---",
+                        "Zink": "393,0",
+                        "Molybdän": "4,53",
+                        "Einheit": "mg/kg TM"
+                    };
+                }
+            },
+            {
+                getMappedProperties: () => {
+                    return {
+                        "Entnahme Datum": "2019",
+                        "OHG in Meter": "0.00",
+                        "UHG in Meter": "0.10",
+                        "Arsen": "14,7",
+                        "Cadmium": "1,34",
+                        "Chrom": "40,5",
+                        "Kupfer": "774,0",
+                        "Quecksilber": "0,346",
+                        "Nickel": "22,9",
+                        "Blei": "209,0",
+                        "Thallium": "---",
+                        "Zink": "568,0",
+                        "Molybdän": "19,8",
+                        "Einheit": "mg/kg TM"
+                    };
+                }
+            }];
+        }
+    };
 
     beforeEach(() => {
+        spyRunSorting = sinon.spy(DataTableTheme.methods, "runSorting");
         wrapper = shallowMount(DataTableTheme, {
-            store,
             localVue,
             propsData: {
                 feature: featureData
@@ -91,62 +86,300 @@ describe("/src/modules/tools/gfi/components/themes/dataTable/components/DataTabl
         });
     });
 
-    it("It should exist a container for a data table", () => {
-        expect(wrapper.find("#table-data-container").exists()).to.be.true;
+    afterEach(() => {
+        sinon.restore();
     });
 
-    it("Check the displayed table head", () => {
-        const theads = wrapper.findAll("#table-data-container table th");
-
-        expect(theads.exists()).to.be.true;
-
-        wrapper.vm.refinedData.head.forEach((singleCaption, index) => {
-            expect(theads.at(index).text()).to.equal(singleCaption);
+    describe("Component DOM", () => {
+        it("It should exist a container for a data table", () => {
+            expect(wrapper.find("#table-data-container").exists()).to.be.true;
         });
-    });
 
-    it("Check the displayed table data", () => {
-        const trs = wrapper.findAll("#table-data-container table tr");
+        it("Check the displayed table head", () => {
+            const theads = wrapper.findAll("#table-data-container table th");
 
-        expect(trs.exists()).to.be.true;
+            expect(theads.exists()).to.be.true;
 
-        trs.wrappers.forEach((tr, indexTr) => {
-            const tds = tr.findAll("td");
-
-            expect(tds.exists()).to.be.true;
-
-            tds.wrappers.forEach((td, indexTd) => {
-                expect(td.text()).to.equal(Object.values(wrapper.vm.refinedData.rows[indexTr])[indexTd]);
+            wrapper.vm.columns.forEach((singleCaption, index) => {
+                expect(theads.at(index).text()).to.equal(singleCaption.name);
             });
         });
-    });
 
-    it("The enableDownload in computed section should be true", () => {
-        expect(wrapper.vm.enableDownload).to.be.true;
-    });
+        it("Check the displayed table data", () => {
+            const trs = wrapper.findAll("#table-data-container table tr");
 
-    it("It should contains a download button", () => {
-        expect(wrapper.find(".download").exists()).to.be.true;
-    });
+            expect(trs.exists()).to.be.true;
 
-    it("It should not contain a download button", () => {
-        featureData = {
-            getTheme: () => "DataTable",
-            getTitle: () => "DataTable",
-            getAttributesToShow: () => {
-                return {};
-            },
-            getMimeType: () => "text/xml",
-            getFeatures: () => []
-        };
-        wrapper = shallowMount(DataTableTheme, {
-            store,
-            localVue,
-            propsData: {
-                feature: featureData
-            }
+            trs.wrappers.forEach((tr, indexTr) => {
+                const tds = tr.findAll("td");
+
+                expect(tds.exists()).to.be.true;
+
+                tds.wrappers.forEach((td, indexTd) => {
+                    expect(td.text()).to.equal(Object.values(wrapper.vm.rows[indexTr])[indexTd]);
+                });
+            });
         });
 
-        expect(wrapper.find(".download").exists()).to.be.false;
+        it("The enableDownload in computed section should be true", () => {
+            expect(wrapper.vm.enableDownload).to.be.true;
+        });
+
+        it("It should contains a download button", () => {
+            expect(wrapper.find(".download").exists()).to.be.true;
+        });
+
+        it("It should not contain a download button", async () => {
+            const newFeature = {
+                getTheme: () => "DataTable",
+                getTitle: () => "DataTable",
+                getAttributesToShow: () => {
+                    return {};
+                },
+                getMimeType: () => "text/xml",
+                getFeatures: () => []
+            };
+
+            await wrapper.setProps({feature: newFeature});
+
+            expect(wrapper.find(".download").exists()).to.be.false;
+        });
+
+        it("should find two bootstrap icons if isSortable is true", () => {
+            const icons = wrapper.findAll(".bootstrap-icon, .bi-arrow-down-up, .origin-order");
+
+            expect(icons.length).to.be.equal(2);
+        });
+
+        it("should not find bootstrap icons if isSortable is false", async () => {
+            const newFeature = {
+                getTheme: () => {
+                    return {
+                        "name": "DataTable",
+                        "params": {
+                            "enableDownload": true,
+                            "isSortable": false
+                        }
+                    };
+                },
+                getAttributesToShow: () => {
+                    return {};
+                },
+                getFeatures: () => []
+            };
+
+            await wrapper.setProps({feature: newFeature});
+
+            expect(wrapper.findAll(".bootstrap-icon, .bi-arrow-down-up, .origin-order").length).to.be.equal(0);
+        });
+    });
+
+    describe("User Interactions", () => {
+        it("should call 'runSorting' when the icon is clicked", async () => {
+            const icon = wrapper.find(".bootstrap-icon");
+
+            await icon.trigger("click");
+            expect(spyRunSorting.calledOnce).to.be.true;
+        });
+    });
+
+    describe("methods", () => {
+        describe("getColumns", () => {
+            it("should create the correct column objects", () => {
+                const columns = wrapper.vm.getColumns({key1: "Moin", key2: "Tschüss"}),
+                    expectedColumns = [{
+                        name: "Moin",
+                        order: "origin",
+                        index: 0
+                    },
+                    {
+                        name: "Tschüss",
+                        order: "origin",
+                        index: 1
+                    }];
+
+
+                expect(columns).to.deep.equal(expectedColumns);
+            });
+        });
+
+        describe("getIconClassByOrder", () => {
+            it("should return the correct icon class for ascending order", () => {
+                const iconClass = wrapper.vm.getIconClassByOrder("asc");
+
+                expect(iconClass).to.be.equal("bi-arrow-up");
+            });
+
+            it("should return the correct icon class for descending order", () => {
+                const iconClass = wrapper.vm.getIconClassByOrder("desc");
+
+                expect(iconClass).to.be.equal("bi-arrow-down");
+            });
+
+            it("should return the correct icon class for origin order", () => {
+                const iconClass = wrapper.vm.getIconClassByOrder("origin");
+
+                expect(iconClass).to.be.equal("bi-arrow-down-up origin-order");
+            });
+        });
+
+        describe("getOldSortedColumn", () => {
+            it("should return the old sorted column when there is a new one to be sorted", () => {
+                const columns = [{
+                        name: "Moin",
+                        order: "asc",
+                        index: 0
+                    },
+                    {
+                        name: "Tschüss",
+                        order: "origin",
+                        index: 1
+                    }],
+                    sortedColumn = wrapper.vm.getOldSortedColumn(columns, 1);
+
+                expect(sortedColumn).to.deep.equal(columns[0]);
+            });
+
+            it("should return undefined if the old sorted column is also the new one", () => {
+                const columns = [{
+                        name: "Moin",
+                        order: "asc",
+                        index: 0
+                    },
+                    {
+                        name: "Tschüss",
+                        order: "origin",
+                        index: 1
+                    }],
+                    sortedColumn = wrapper.vm.getOldSortedColumn(columns, 0);
+
+                expect(sortedColumn).to.be.undefined;
+            });
+
+            it("should return undefined if there is no sorted column", () => {
+                const columns = [{
+                        name: "Moin",
+                        order: "origin",
+                        index: 0
+                    },
+                    {
+                        name: "Tschüss",
+                        order: "origin",
+                        index: 1
+                    }],
+                    sortedColumn = wrapper.vm.getOldSortedColumn(columns, 0);
+
+                expect(sortedColumn).to.be.undefined;
+            });
+        });
+
+        describe("getSortedRows", () => {
+            it("should return the 'originRows' if the rows are to be sorted in their origin order", () => {
+                const originRows = wrapper.vm.getSortedRows([], "origin");
+
+                expect(originRows).to.deep.equal(wrapper.vm.originRows);
+            });
+
+            it("should return the rows in ascending order sorted by name ", () => {
+                const rows = [
+                        {
+                            "name": "klm"
+                        },
+                        {
+                            "name": "xyz"
+                        },
+                        {
+                            "name": "abc"
+                        }
+                    ],
+                    expectRows = [
+                        {
+                            "name": "abc"
+                        },
+                        {
+                            "name": "klm"
+                        },
+                        {
+                            "name": "xyz"
+                        }
+                    ],
+                    sortedRows = wrapper.vm.getSortedRows(rows, "asc", "name");
+
+                expect(sortedRows).to.deep.equal(expectRows);
+            });
+
+            it("should return the rows in descending order sorted by name ", () => {
+                const rows = [
+                        {
+                            "name": "klm"
+                        },
+                        {
+                            "name": "xyz"
+                        },
+                        {
+                            "name": "abc"
+                        }
+                    ],
+                    expectRows = [
+                        {
+                            "name": "xyz"
+                        },
+                        {
+                            "name": "klm"
+                        },
+                        {
+                            "name": "abc"
+                        }
+                    ],
+                    sortedRows = wrapper.vm.getSortedRows(rows, "desc", "name");
+
+                expect(sortedRows).to.deep.equal(expectRows);
+            });
+        });
+
+        describe("getSortOrder", () => {
+            it("should return 'desc' order when passed 'asc'", () => {
+                const order = wrapper.vm.getSortOrder("asc");
+
+                expect(order).to.be.equal("desc");
+            });
+
+            it("should return 'origin' order when passed 'desc'", () => {
+                const order = wrapper.vm.getSortOrder("desc");
+
+                expect(order).to.be.equal("origin");
+            });
+
+            it("should return 'asc' order when passed 'origin'", () => {
+                const order = wrapper.vm.getSortOrder("origin");
+
+                expect(order).to.be.equal("asc");
+            });
+        });
+
+        describe("runSorting", () => {
+            it("should set the sort order for the columns correctly", () => {
+                wrapper.vm.runSorting(wrapper.vm.columns[0]);
+
+                expect(wrapper.vm.columns[0].order).to.be.equal("asc");
+                expect(wrapper.vm.columns[1].order).to.be.equal("origin");
+            });
+
+            it("should set the sort order for the columns correctly", () => {
+                wrapper.vm.columns[1].order = "asc";
+                wrapper.vm.runSorting(wrapper.vm.columns[0]);
+
+                expect(wrapper.vm.columns[0].order).to.be.equal("asc");
+                expect(wrapper.vm.columns[1].order).to.be.equal("origin");
+            });
+
+            it("should set the sort order for the columns correctly", () => {
+                wrapper.vm.columns[1].order = "asc";
+                wrapper.vm.runSorting(wrapper.vm.columns[1]);
+
+                expect(wrapper.vm.columns[0].order).to.be.equal("origin");
+                expect(wrapper.vm.columns[1].order).to.be.equal("desc");
+            });
+        });
     });
 });
