76ed06b6fe26b6330decc20d7895408f3b40905b
Julien Hofer <julien.hofer@gv.hamburg.de>
2023-04-25 11:33:08 +0200

    Unit tests for getters and default values

---
+++ vpiDashboard/test/unit/vpidashboard.spec.js
@@ -0,0 +1,46 @@
+import {expect} from "chai";
+import getters from "../../store/gettersVpiDashboard";
+import stateVpiDashboard from "../../store/stateVpiDashboard";
+
+
+const {
+    active,
+    id,
+    name,
+    icon,
+    renderToWindow,
+    resizableWindow,
+    isVisibleInMenu,
+    deactivateGFI
+} = getters;
+
+describe.only("ADDON: DPI Dashboard", () => {
+    describe("Vpi Dashboard getters", () => {
+        it("returns the active state from state", () => {
+            expect(active(stateVpiDashboard)).to.be.false;
+        });
+        it("returns the id from state", () => {
+            expect(id(stateVpiDashboard)).to.equals("vpiDashboard");
+        });
+    });
+    describe("testing default values", () => {
+        it("returns the name default value from state", () => {
+            expect(name(stateVpiDashboard)).to.be.equals("translate#additional:modules.tools.vpiDashboard.title");
+        });
+        it("returns the icon default value from state", () => {
+            expect(icon(stateVpiDashboard)).to.equals("bi-graph-up");
+        });
+        it("returns the renderToWindow default value from state", () => {
+            expect(renderToWindow(stateVpiDashboard)).to.be.true;
+        });
+        it("returns the resizableWindow default value from state", () => {
+            expect(resizableWindow(stateVpiDashboard)).to.be.true;
+        });
+        it("returns the isVisibleInMenu default value from state", () => {
+            expect(isVisibleInMenu(stateVpiDashboard)).to.be.true;
+        });
+        it("returns the deactivateGFI default value from state", () => {
+            expect(deactivateGFI(stateVpiDashboard)).to.be.false;
+        });
+    });
+});
