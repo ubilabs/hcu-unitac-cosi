863fe2e5f7b309e774dadc1b4c9774ce30ccef92
YilmazBi-lokal <YilmazBi-lokal@LGV-W-D6B8244.fhhnet.stadt.hamburg.de>
2023-06-08 14:39:41 +0200

    VPI-88: Loader is now only shown in VPI Dashboard

--- vpiDashboard/components/VpiLoader.vue
+++ vpiDashboard/components/VpiLoader.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         id="vpiDashboardLoader"
-        class="centered-box-wrapper loader-is-loading loader-is-initially-loading"
+        class="centered-box-wrapper loader-is-initially-loading"
     >
         <div class="centered-box-content">
             <div
@@ -13,7 +13,6 @@
 </template>
 
 <style scoped>
-/* Loader Overlay */
 #vpiDashboardLoader {
     position:absolute;
     top:0;
@@ -31,40 +30,15 @@
 #vpiDashboardLoader.loader-is-loading {
     display:block;
 }
-#generic-masterportal-logo {
-    background-color:rgba(255,255,255,0.8);
-    opacity:1;
-    padding: 20px 20px 10px 20px;
-    position: absolute;
-    text-align:center;
-    transition:opacity 0.35s 3s;
-    width:170px;
-}
-#generic-masterportal-logo.initial-fadeout-animation {
-    opacity:0;
-}
-#generic-masterportal-logo img {
-    width:100%;
-}
+
 @media (max-width: 1023px) {
     .vpi-loader-spinner-itself.default {
         border: 5px solid #AAAAAA;
-    }
-    #generic-masterportal-logo {
-        bottom: 15%;
-        left:50%;
-        margin-left:-85px;
-        border-radius: 14px;
     }
 }
 @media (min-width: 1024px) {
     #vpi-loader-spinner-itself.default {
         border: 8px solid #AAAAAA;
-    }
-    #generic-masterportal-logo {
-        bottom: 0;
-        right: 0;
-        border-radius: 14px 0 0 0;
     }
 }
 #vpi-loader-spinner-itself.default {
@@ -99,31 +73,5 @@
         transform: rotate(360deg);
     }
 }
-#portal-logo-box {
-    opacity:1;
-    transition:opacity 0.35s 3s;
-    background-color:rgba(255,255,255,0.8);
-    font-family: "MasterPortalFont Bold", "Arial Narrow", Arial, sans-serif;
-    font-size:36px;
-    margin-bottom:24px;
-    padding:15px 40px 30px 40px;
-    border-radius:14px;
-    text-align:center;
-}
-@media (max-width: 767px) {
-    #portal-logo-box {
-        padding:15px 10px 30px 10px;
-    }
-}
-#portal-logo-box.initial-fadeout-animation {
-    opacity:0;
-}
-#portal-logo-main-title {
-    margin:10px 0 8px 0;
-}
-#portal-logo-sub-title {
-    font-size:0.75em;
-    margin:8px 0 0 0;
-}
-/* End "Loader Overlay" */
+
 </style>
