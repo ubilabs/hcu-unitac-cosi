2418e08b0953bb8f8cd681311fe9dc139732c6cf
Elisabeth Hölscher <elisabeth.hoelscher@gmail.com>
2023-06-15 11:43:28 +0200

    Add translations to SaveSession

--- cosi/SaveSession/components/SaveSession.vue
+++ cosi/SaveSession/components/SaveSession.vue
@@ -433,11 +433,12 @@ export default {
                     />
                     <v-container class="flex btn-grid">
                         <v-card-title secondary-title>
-                            Schnelles Speichern
+                            {{ $t('additional:modules.tools.cosi.saveSession.fastSavingTitle') }}
                         </v-card-title>
                         <div class="mb-2">
-                            Sitzungen im Browser (z.B. Edge, Firefox) speichern. Diese können beim Start von CoSI über den Button 'Letzte Laden' wieder geladen werden. <br>
-                            Wenn Browserverlauf oder Cache geleert werden, geht dieser Speicherstand verloren! Es kann immer nur eine Sitzung vorgehalten werden.
+                            {{ $t('additional:modules.tools.cosi.saveSession.fastSavingDescription1') }}
+                            <br>
+                            {{ $t('additional:modules.tools.cosi.saveSession.fastSavingDescription2') }}
                         </div>
                         <v-row class="flex">
                             <v-col
@@ -510,10 +511,10 @@ export default {
                         </v-row>
                         <v-divider />
                         <v-card-title secondary-title>
-                            Lokales Speichern
+                            {{ $t('additional:modules.tools.cosi.saveSession.localSavingTitle') }}
                         </v-card-title>
                         <div class="mb-2">
-                            Sitzungen als Datei auf dem Rechner speichern und über den Button 'Datei laden' wieder laden. Diese können jederzeit wieder geladen oder mit anderen CoSI Nutzer:innen geteilt werden.
+                            {{ $t('additional:modules.tools.cosi.saveSession.localSavingDescription') }}
                         </div>
                         <v-row class="flex">
                             <v-col
@@ -570,7 +571,7 @@ export default {
                             dense
                         >
                             <small>
-                                Bitte beachten Sie, dass nicht alle Daten und Aktionen gespeichert werden können. Grundsätzlich bezieht sich das Speichern auf die Gebiets- und Themenauswahl, Analyse- und Simulationsergebnisse. Welche Daten gespeichert werden können entnehmen Sie bitte der Anleitung.
+                                {{ $t('additional:modules.tools.cosi.saveSession.localSavingHint') }}
                             </small>
                         </v-row>
                     </v-container>
